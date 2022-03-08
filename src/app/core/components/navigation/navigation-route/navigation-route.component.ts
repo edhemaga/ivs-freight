62208443caede705819a2e9bc3276cb76a94e196
MarkoJoker <djordjevicit97@gmail.com>
2022-03-08 11:12:23 +0100

    Navigation - delete some dummy data for nav-route

--- src/app/core/components/navigation/navigation-route/navigation-route.component.ts
+++ src/app/core/components/navigation/navigation-route/navigation-route.component.ts
@@ -24,41 +24,26 @@ export class NavigationRouteComponent implements OnInit {
     this.timeout = setTimeout(() => {
       this.isActiveRouteOnReload(window.location.href);
       clearTimeout(this.timeout);
-    }, 1000)
-    
+    }, 1000);
   }
 
   public onRouteAction() {
     localStorage.removeItem('subroute_active');
     this.onRouteEvent.emit({
       routeId: this.route.id,
-      routes: this.route.route
+      routes: this.route.route,
     });
-    
-    if(!Array.isArray(this.route.route)) {
+
+    if (!Array.isArray(this.route.route)) {
       this.router.navigate([`${this.route.route}`]);
     }
-    // TODO: RADI KAD JE RELOAD, ALI DA LI RADI KAD SE KLIKNE ??
-    // if (Array.isArray(this.route.route)) {
-    //   if (this.route.isRouteActive) {
-    //     if (!flegId) {
-    //       flegId = JSON.parse(localStorage.getItem('route_flegId'));
-    //     }
-    //     const subroute = this.route.route.find(
-    //       (item) => item.flegId === flegId
-    //     );
-    //     this.router.navigate([`${subroute?.route}`]);
-    //   }
-    // } else {
-    //   this.router.navigate([`${this.route.route}`]);
-    // }
   }
 
   public onReloadSubroute(flegId?: number) {
     this.onRouteEvent.emit({
       routeId: this.route.id,
       routes: this.route.route,
-      activeRouteFlegId: flegId
+      activeRouteFlegId: flegId,
     });
   }
 
@@ -68,30 +53,16 @@ export class NavigationRouteComponent implements OnInit {
 
     const flegId = JSON.parse(localStorage.getItem('subroute_active'));
 
-    if(flegId && this.route.id === flegId) {
-        console.log("FROM ROUTING ", this.route)
-        this.onReloadSubroute(flegId);
+    if (flegId && this.route.id === flegId) {
+      console.log('FROM ROUTING ', this.route);
+      this.onReloadSubroute(flegId);
     }
-    
-    if (!Array.isArray(this.route.route) && this.route.route.includes(reloadUrl)) {
+
+    if (
+      !Array.isArray(this.route.route) &&
+      this.route.route.includes(reloadUrl)
+    ) {
       this.onRouteAction();
-    } 
-    // else {
-    //   const subroute = this.route.route.find((item) =>
-    //     item.route.includes(reloadUrl)
-    //   );
-      // if (subroute) {
-      //   if (this.route.id === subroute.activeRouteFlegId) {
-      //     this.route.isRouteActive = true;
-      //     this.isActiveSubroute = true;
-      //     localStorage.removeItem('route_flegId');
-      //     localStorage.setItem(
-      //       'route_flegId',
-      //       subroute.activeRouteFlegId.toString()
-      //     );
-      //     this.onRouteAction(subroute.activeRouteFlegId);
-      //   }
-      // }
-   // }
+    }
   }
 }
