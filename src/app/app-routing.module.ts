8d60456c5c52b35e747453d5081bea491103b7c3
Savke97 <savkeren97@gmail.com>
2022-03-22 14:35:44 +0100

    truck component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,44 +1,61 @@
 import { DispatcherResolverService } from './core/components/dispatcher/state/dispatcher-resolver.service';
 import { DashboardResolverService } from './core/components/dashboard/state/dashboard-resolver.service';
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/authentication.guard';
 
 const routes: Routes = [
   // Auth Routes
   {
     path: 'login',
-    loadChildren: () => import('./core/components/authentication/auth.module').then((m) => m.AuthModule)
+    loadChildren: () =>
+      import('./core/components/authentication/auth.module').then(
+        (m) => m.AuthModule
+      ),
   },
   {
     path: 'dashboard',
-    loadChildren: () => import('./core/components/dashboard/dashboard.module').then((m) => m.DashboardModule),
+    loadChildren: () =>
+      import('./core/components/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
     canActivate: [AuthGuard],
-    resolve: { dashboard: DashboardResolverService }
+    resolve: { dashboard: DashboardResolverService },
   },
   {
     path: 'dispatcher',
-    loadChildren: () => import('./core/components/dispatcher/dispatcher.module').then((m) => m.DispatcherModule),
+    loadChildren: () =>
+      import('./core/components/dispatcher/dispatcher.module').then(
+        (m) => m.DispatcherModule
+      ),
     canActivate: [AuthGuard],
-    resolve: {dispatcher: DispatcherResolverService}
+    resolve: { dispatcher: DispatcherResolverService },
   },
   {
     path: 'settings',
-    loadChildren: () => import('./core/components/settings/settings.module').then((m) => m.SettingsModule),
+    loadChildren: () =>
+      import('./core/components/settings/settings.module').then(
+        (m) => m.SettingsModule
+      ),
     canActivate: [AuthGuard],
   },
   {
     path: 'driver',
-    loadChildren: () => import('./core/components/driver/driver.module').then((m) => m.DriverModule),
+    loadChildren: () =>
+      import('./core/components/driver/driver.module').then(
+        (m) => m.DriverModule
+      ),
   },
-  {path: '**', redirectTo: 'dashboard'}
+  {
+    path: 'truck',
+    loadChildren: () =>
+      import('./core/components/truck/truck.module').then((m) => m.TruckModule),
+  },
+  { path: '**', redirectTo: 'dashboard' },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
