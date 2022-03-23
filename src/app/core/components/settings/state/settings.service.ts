96ca3042676670f886e5b3b9141c1263b5e82f5b
MarkoJoker <djordjevicit97@gmail.com>
2022-03-23 13:57:01 +0100

    Settings - change logic for modals opening

--- src/app/core/components/settings/state/settings.service.ts
+++ src/app/core/components/settings/state/settings.service.ts
@@ -1,15 +1,55 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 import { SettingsStore } from './settings.store';
+import { CustomModalService } from 'src/app/core/services/modals/custom-modal.service';
+import { SettingsBasicModalComponent } from '../settings-company/company-modals/settings-basic-modal/settings-basic-modal.component';
+import { SettingsInsurancePolicyModalComponent } from '../settings-company/company-modals/settings-insurance-policy-modal/settings-insurance-policy-modal.component';
+import { SettingsFactoringModalComponent } from '../settings-company/company-modals/settings-factoring-modal/settings-factoring-modal.component';
 
 @Injectable({ providedIn: 'root' })
 export class SettingsStoreService {
 
-  public modalSubject$ = new BehaviorSubject<{modalName: string, type: boolean, action: string}>({modalName: null, type: false, action: null});
+  public isModalActive$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor(private settingsStore: SettingsStore, private http: HttpClient) {}
+  constructor(
+    private settingsStore: SettingsStore,
+    private customModalService: CustomModalService,
+    private http: HttpClient
+  ) {}
 
+
+  /**
+   * Open Modal
+   * @param type - is modal active
+   * @param modalName - modal name
+   * @param action - type of tab-switcher to be active
+   */
+  public onModalAction(type: boolean, modalName: string, action?: string) {
+    if (type && modalName === 'basic') {
+      this.customModalService.openModal(
+        SettingsBasicModalComponent,
+        null,
+        null,
+        { size: 'small' }
+      );
+    } else if (type && modalName === 'insurance-policy') {
+      console.log(modalName);
+      this.customModalService.openModal(
+        SettingsInsurancePolicyModalComponent,
+        null,
+        null,
+        { size: 'small' }
+      );
+    } else if (type && modalName === 'factoring') {
+      this.customModalService.openModal(
+        SettingsFactoringModalComponent,
+        null,
+        null,
+        { size: 'small' }
+      );
+    }
+    this.isModalActive$.next(true);
+  }
 }
