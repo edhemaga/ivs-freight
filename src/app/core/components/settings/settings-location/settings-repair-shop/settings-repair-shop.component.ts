16504df2f6dcf8aa1a3c49c4e88d89bbae99c9fd
MarkoJoker <djordjevicit97@gmail.com>
2022-03-24 14:53:04 +0100

    Settings - add svgs - implement repair shop and terminal

--- src/app/core/components/settings/settings-location/settings-repair-shop/settings-repair-shop.component.ts
+++ src/app/core/components/settings/settings-location/settings-repair-shop/settings-repair-shop.component.ts
@@ -7,11 +7,147 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsRepairShopComponent implements OnInit {
 
+  public repairShopData = [
+    {
+      id: 1,
+      name: 'IVS REPAIR SHOP',
+      phone: '(123) 456-7890',
+      email: '',
+      address: '5462 N East River Rd apt 611 Chicago, IL 60656',
+      companyOwned: 'assets/img/svgs/settings-company/settings-company-owned.svg',
+      services: [
+        {
+          id: 1,
+          name: 'Truck',
+          svg: 'assets/img/svgs/settings-company/settings-truck.svg',
+          isUsefulService: true
+        },
+        {
+          id: 2,
+          name: 'Trailer',
+          svg: 'assets/img/svgs/settings-company/settings-trailer.svg',
+          isUsefulService: false
+        },
+        {
+          id: 3,
+          name: 'Mobile',
+          svg: 'assets/img/svgs/settings-company/settings-mobile.svg',
+          isUsefulService: false
+        },
+        {
+          id: 4,
+          name: 'Shop',
+          svg: 'assets/img/svgs/settings-company/settings-shop.svg',
+          isUsefulService: false
+        },
+        {
+          id: 5,
+          name: 'Towing',
+          svg: 'assets/img/svgs/settings-company/settings-towing.svg',
+          isUsefulService: true
+        },
+        {
+          id: 6,
+          name: 'Parts',
+          svg: 'assets/img/svgs/settings-company/settings-parts.svg',
+          isUsefulService: false
+        },
+        {
+          id: 7,
+          name: 'Tire',
+          svg: 'assets/img/svgs/settings-company/settings-tire.svg',
+          isUsefulService: true
+        },
+        {
+          id: 9,
+          name: 'Dealer',
+          svg: 'assets/img/svgs/settings-company/settings-dealer.svg',
+          isUsefulService: true
+        }
+      ],
+      rent: '',
+      pay_period: '',
+      day: '',
+    },
+    {
+      id: 2,
+      name: 'KSKA TRUCK REPAIRS',
+      phone: '(123) 456-7890',
+      email: 'contact@windsor-brokers.com',
+      address: '5462 N East River Rd apt 611 Chicago, IL 60656',
+      companyOwned: 'assets/img/svgs/settings-company/settings-company-owned.svg',
+      services: [
+        {
+          id: 1,
+          name: 'Truck',
+          svg: 'assets/img/svgs/settings-company/settings-truck.svg',
+          isUsefulService: true
+        },
+        {
+          id: 2,
+          name: 'Trailer',
+          svg: 'assets/img/svgs/settings-company/settings-trailer.svg',
+          isUsefulService: true
+        },
+        {
+          id: 3,
+          name: 'Mobile',
+          svg: 'assets/img/svgs/settings-company/settings-mobile.svg',
+          isUsefulService: false
+        },
+        {
+          id: 4,
+          name: 'Shop',
+          svg: 'assets/img/svgs/settings-company/settings-shop.svg',
+          isUsefulService: true
+        },
+        {
+          id: 5,
+          name: 'Towing',
+          svg: 'assets/img/svgs/settings-company/settings-towing.svg',
+          isUsefulService: true
+        },
+        {
+          id: 6,
+          name: 'Parts',
+          svg: 'assets/img/svgs/settings-company/settings-parts.svg',
+          isUsefulService: true
+        },
+        {
+          id: 7,
+          name: 'Tire',
+          svg: 'assets/img/svgs/settings-company/settings-tire.svg',
+          isUsefulService: true
+        },
+        {
+          id: 9,
+          name: 'Dealer',
+          svg: 'assets/img/svgs/settings-company/settings-dealer.svg',
+          isUsefulService: false
+        }
+      ],
+      rent: '$350.000',
+      pay_period: 'Weekly',
+      day: '25th',
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
   }
   public onAction(data: { modalName: string, type: boolean, action: string }) {
     
+  }
+
+  public identity(index: number, item: any): number {
+    return item.id;
+  }
+
+  public generateTextForProgressBar(data: any): string {
+    return data.pay_period + ' Rent ' + `- ${data.rent}`
+  }
+
+  public getActiveServices(services: any[]): any[] {
+    return services.filter(item => item.isUsefulService)
   }
 }
--- src/app/core/components/settings/settings-location/settings-terminal/settings-terminal.component.ts
+++ src/app/core/components/settings/settings-location/settings-terminal/settings-terminal.component.ts
@@ -3,15 +3,137 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-settings-terminal',
   templateUrl: './settings-terminal.component.html',
-  styleUrls: ['./settings-terminal.component.scss']
+  styleUrls: ['./settings-terminal.component.scss'],
 })
 export class SettingsTerminalComponent implements OnInit {
+  public terminalData = [
+    {
+      id: 1,
+      name: 'IVS DRIVER CENTER',
+      phone: '(123) 456-7890',
+      email: 'contact@windsor-brokers.com',
+      address: '5462 N East River Rd apt 611 Chicago, IL 60656',
+      companyOwned:
+        'assets/img/svgs/settings-company/settings-company-owned.svg',
+      options: [
+        {
+          id: 1,
+          name: 'Office',
+          check: true,
+          phone: '(123) 456-7890',
+          phone_ext: '1234',
+          email: 'contact@windsor-brokers.com',
+        },
+        {
+          id: 2,
+          name: 'Dispatch Department',
+          check: true,
+          phone: '(123) 456-7890',
+          phone_ext: '1234',
+          email: 'contact@windsor-brokers.com',
+        },
+        {
+          id: 3,
+          name: 'Safety Department',
+          check: true,
+          phone: '(123) 456-7890',
+          phone_ext: '1234',
+          email: 'contact@windsor-brokers.com',
+        },
+        {
+          id: 4,
+          name: 'Warehouse',
+          check: true,
+          phone: '(123) 456-7890',
+          phone_ext: '1234',
+          email: 'contact@windsor-brokers.com',
+        },
+        {
+          id: 5,
+          name: 'Parking',
+          check: true,
+          phone: '(123) 456-7890',
+          parking_slot: 12,
+          parking_slot_full: 5,
+          gate: true,
+          security_camera: true,
+        },
+        {
+          id: 6,
+          name: 'Fuel Station',
+          check: true
+        }
+      ],
+    },
+    {
+      id: 1,
+      name: 'Alexandro Castor Office',
+      phone: '(123) 456-7890',
+      email: 'contact@windsor-brokers.com',
+      address: 'East River Rd apt 611, Chicago, IL 60656 ',
+      companyOwned: '',
+      options: [
+        {
+          id: 1,
+          name: 'Office',
+          check: false,
+          phone: '',
+          phone_ext: '',
+          email: '',
+        },
+        {
+          id: 2,
+          name: 'Dispatch Department',
+          check: true,
+          phone: '(123) 456-7890',
+          phone_ext: '1234',
+          email: 'contact@windsor-brokers.com',
+        },
+        {
+          id: 3,
+          name: 'Safety Department',
+          check: false,
+          phone: '',
+          phone_ext: '',
+          email: '',
+        },
+        {
+          id: 4,
+          name: 'Warehouse',
+          check: true,
+          phone: '(123) 456-7890',
+          phone_ext: '1234',
+          email: 'contact@windsor-brokers.com',
+        },
+        {
+          id: 5,
+          name: 'Parking',
+          check: true,
+          phone: '(123) 456-7890',
+          parking_slot: 12,
+          parking_slot_full: 5,
+          gate: true,
+          security_camera: true,
+        },
+        {
+          id: 6,
+          name: 'Fuel Station',
+          check: false
+        }
+      ],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  public onAction(data: { modalName: string; type: boolean; action: string }) {}
+
+  public identityTerminalData(index: number, item: any): number {
+    return item.id;
   }
-  public onAction(data: { modalName: string, type: boolean, action: string }) {
-    
+
+  public identityCardData(index: number, item: any): number {
+    return item.id;
   }
 }
