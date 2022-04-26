105f6bd6dbf3a70ca7c2c26fe2411b2fe7ebd423
MarkoJoker <djordjevicit97@gmail.com>
2022-04-26 09:08:50 +0200

    Email validation fix

--- src/app/core/components/modals/driver-modal/driver-modal.component.ts
+++ src/app/core/components/modals/driver-modal/driver-modal.component.ts
@@ -116,7 +116,7 @@ export class DriverModalComponent implements OnInit, OnDestroy {
         null,
         [Validators.required, Validators.pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/)],
       ],
-      email: [null, [Validators.required, Validators.email]],
+      email: [null, [Validators.required, Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
       ssn: [
         null,
         [Validators.required, Validators.pattern(/^\d{3}\-\d{2}\-\d{4}$/)],
