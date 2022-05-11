a85e9a192681bb1f5ab8b6e02284887f01a53f23
Milos Cirkovic <cirkovicmilos99@gmail.com>
2022-05-11 15:49:28 +0200

    Resolve conflicts

--- src/app/core/components/driver/state/driver.store.ts
+++ src/app/core/components/driver/state/driver.store.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
 import { DriverShortResponse } from 'appcoretruckassist';
-import { Driver } from './driver.model';
 
-export interface DriversState extends EntityState<Driver, number> {}
+export interface DriversState extends EntityState<DriverShortResponse[], number> {}
 
 @Injectable({ providedIn: 'root' })
 @StoreConfig({ name: 'drivers' })
