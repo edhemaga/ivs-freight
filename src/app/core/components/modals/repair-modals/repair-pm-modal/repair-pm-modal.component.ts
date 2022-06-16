bf7fb0b20b24a843f0ef1b52f0a9c6a20e37ebbc
Marko Joker <djordjevicit97@gmail.com>
2022-06-16 08:36:45 +0200

    Repair pm

--- src/app/core/components/modals/repair-modals/repair-pm-modal/repair-pm-modal.component.ts
+++ src/app/core/components/modals/repair-modals/repair-pm-modal/repair-pm-modal.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-repair-pm-modal',
   templateUrl: './repair-pm-modal.component.html',
-  styleUrls: ['./repair-pm-modal.component.scss']
+  styleUrls: ['./repair-pm-modal.component.scss'],
 })
 export class RepairPmModalComponent implements OnInit {
+  @Input() editData: any;
 
-  constructor() { }
+  public pmForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.createForm();
   }
 
+  private createForm() {
+    this.pmForm = this.formBuilder.group({
+      pms: this.formBuilder.array([]),
+    });
+  }
+
+  public get pms(): FormArray {
+    return this.pmForm.get('pms') as FormArray;
+  }
+
+  private createPMS(
+    isChecked: boolean = false,
+    title: string,
+    miles: number
+  ): FormGroup {
+    return this.formBuilder.group({
+      isChecked: [isChecked],
+      title: [title],
+      miles: [miles],
+    });
+  }
+
+  public addPMS(event: any) {
+    if (event) {
+      this.pms.push(this.createPMS(false, 'Engine Oil & Filter', 15000));
+    }
+  }
+
+  public removeTrucksPM(id: number) {
+    this.pms.removeAt(id);
+  }
+
+  public onModalAction(data: { action: string; bool: boolean }) {}
+
+  public addPM(event) {
+    this.addPMS(true);
+  }
 }
