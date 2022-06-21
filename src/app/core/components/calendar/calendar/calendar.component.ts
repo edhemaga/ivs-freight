11a57ab284571d93d6bf7b7f874f66f0e04b9598
sslavishaa <sslavishaa@gmail.com>
2022-06-21 15:53:52 +0200

    calnedar title

--- src/app/core/components/calendar/calendar/calendar.component.ts
+++ src/app/core/components/calendar/calendar/calendar.component.ts
@@ -5,7 +5,7 @@ import { CalendarOptions, FullCalendarComponent } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 
-import { start } from 'repl';
+import moment from 'moment';
 
 @Component({
   selector: 'app-calendar',
@@ -785,14 +785,15 @@ export class CalendarComponent implements OnInit {
 
   public checkCalendarTitle() {
     const calendarApi = this.fullcalendar.getApi();
-    console.log(calendarApi.currentData, 'viewitle');
-    if ( this.calendarGridView == 'timeGridWeek' ) {
-      this.calendarTitle = calendarApi.currentData.viewTitle.split(",")[0];
+    console.log("calendarApi");
+    console.log(calendarApi.currentData.currentDate); 
+
+    console.log(moment(calendarApi.currentData.currentDate).format("MMMM D, YYYY"));
+
+    if(this.currentCalendarView === "day"){
+      this.calendarTitle = moment(calendarApi.currentData.currentDate).format("MMMM D, YYYY");
     }
-    else{
-      var calendarTitleParts = calendarApi.currentData.viewTitle.split(" ");
-      this.calendarTitle = calendarTitleParts[0]+', '+calendarTitleParts[1];
-    }
+
   }
 
   resizeEvent(mod) {
@@ -839,11 +840,11 @@ export class CalendarComponent implements OnInit {
     calendarApi.changeView(view);
     console.log(calendarApi.currentData.viewTitle, 'calendarApi.currentData.viewTitle');
     if ( this.calendarGridView == 'timeGridWeek' ) {
-      this.calendarTitle = calendarApi.currentData.viewTitle.split(",")[0];
+      //this.calendarTitle = calendarApi.currentData.viewTitle.split(",")[0];
     }
     else{
       var calendarTitleParts = calendarApi.currentData.viewTitle.split(" ");
-      this.calendarTitle = calendarTitleParts[0]+', '+calendarTitleParts[1];
+      //this.calendarTitle = calendarTitleParts[0]+', '+calendarTitleParts[1];
     }
     
     if ( this.calendarGridView == 'timeGridWeek' ) {
