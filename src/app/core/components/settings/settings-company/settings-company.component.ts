70f9033ce5642f7a63fcec70ea3b65dacccbab27
Milos Cirkovic <cirkovicmilos99@gmail.com>
2022-07-06 13:21:46 +0200

    Update Company Settings

--- src/app/core/components/settings/settings-company/settings-company.component.ts
+++ src/app/core/components/settings/settings-company/settings-company.component.ts
@@ -16,6 +16,7 @@ import { DetailsPageService } from 'src/app/core/services/details-page/details-p
 import { NotificationService } from 'src/app/core/services/notification/notification.service';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { CompanyResponse } from 'appcoretruckassist';
+import { CompanyQuery } from '../state/company-state/company-settings.query';
 @Component({
   selector: 'app-settings-company',
   templateUrl: './settings-company.component.html',
@@ -27,7 +28,8 @@ export class SettingsCompanyComponent implements OnInit, OnChanges, OnDestroy {
   public isModalOpen$: boolean; // TODO: FILL DATA WITH REAL DATA, IF NO DATA, SHOW NO_DATA_COMPONENT !!!
   public data: any;
   public dataDivison: any;
-  public optionsCmp:any
+  public optionsCmp: any;
+  public dataCompany: any;
   constructor(
     private settingsStoreService: SettingsStoreService,
     private activated: ActivatedRoute,
@@ -36,12 +38,14 @@ export class SettingsCompanyComponent implements OnInit, OnChanges, OnDestroy {
     private cdRef: ChangeDetectorRef
   ) {}
   ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
   ngOnInit(): void {
     this.getData(this.activated.snapshot.data.company);
+    this.getCompanyDivision();
     this.detailsPageSer.pageDetailChangeId$
       .pipe(untilDestroyed(this))
-      .subscribe((id) => { 
+      .subscribe((id) => {
         this.settingsStoreService
           .getCompanyDivisionById(id)
           .pipe(untilDestroyed(this))
@@ -49,15 +53,16 @@ export class SettingsCompanyComponent implements OnInit, OnChanges, OnDestroy {
             next: (res: CompanyResponse) => {
               this.getData(res);
               this.notificationService.success(
-                'Company successfully changed',
+                'Company Division successfully changed',
                 'Success:'
               );
               this.cdRef.detectChanges();
             },
             error: () => {
-              this.notificationService.error(
-                "Company can't be loaded",
-                'Error:'
+              this.getData(this.activated.snapshot.data.company);
+              this.notificationService.success(
+                'Company successfully changed',
+                'Success:'
               );
             },
           });
@@ -70,33 +75,31 @@ export class SettingsCompanyComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   public getData(data: CompanyResponse) {
-    this.data = data;    
-  }
-
-  public toggle(event:any){
-    this.getCompanyDivision()
+    this.data = data;
   }
   public getCompanyDivision() {
-    this.optionsCmp = this.activated.snapshot.data.company.divisions.map((item) => {
-      return {
-        ...item,
-        id: item.id,
-        data: item,
-        name: item.companyName,
-        active: item.id,
-      };
-    })
+    this.optionsCmp = this.activated.snapshot.data.company.divisions.map(
+      (item) => {
+        return {
+          ...item,
+          id: item.id,
+          name: item.companyName,
+          active: !item.isDivision,
+        };
+      }
+    );
   }
   public selectCompany(event: any) {
-  
-   this.optionsCmp= this.activated.snapshot.data.company.divisions.map((item) => {
-      return {
-        ...item,
-        id: item.id,
-        name: item.companyName,
-        active: item.id === event.id,
-      };
-    });
+    this.optionsCmp = this.activated.snapshot.data.company.divisions.map(
+      (item) => {
+        return {
+          ...item,
+          id: item.id,
+          name: item.companyName,
+          active: item.id === event.id,
+        };
+      }
+    );
     this.detailsPageSer.getDataDetailId(event.id);
   }
   ngOnDestroy(): void {}
