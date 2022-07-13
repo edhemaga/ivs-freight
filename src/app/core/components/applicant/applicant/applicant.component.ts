d262adc61c2c291ab977b2a465f9b19f67d6adcc
Kvelail <talikadic@gmail.com>
2022-07-13 12:03:59 +0200

    steps header

--- src/app/core/components/applicant/applicant/applicant.component.ts
+++ src/app/core/components/applicant/applicant/applicant.component.ts
@@ -66,9 +66,23 @@ export class ApplicantComponent implements OnInit {
     },
   ];
 
-  public trackByIdentity = (index: number, item: any): number => index;
+  storeArr = [
+    { id: 1, isCompleted: false },
+    { id: 2, isCompleted: false },
+    { id: 3, isCompleted: true },
+    { id: 4, isCompleted: false },
+    { id: 5, isCompleted: true },
+    { id: 6, isCompleted: false },
+    { id: 7, isCompleted: true },
+    { id: 8, isCompleted: false },
+    { id: 9, isCompleted: true },
+    { id: 10, isCompleted: false },
+    { id: 11, isCompleted: true },
+  ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public trackByIdentity = (index: number, item: any): number => index;
 }
