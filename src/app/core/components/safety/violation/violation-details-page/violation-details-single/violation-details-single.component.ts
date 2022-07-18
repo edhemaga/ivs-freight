bcc9445b859065b20a846934e12e2bc022bf8bde
Milos Cirkovic <cirkovicmilos99@gmail.com>
2022-07-18 13:13:41 +0200

    Finish violation

--- src/app/core/components/safety/violation/violation-details-page/violation-details-single/violation-details-single.component.ts
+++ src/app/core/components/safety/violation/violation-details-page/violation-details-single/violation-details-single.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { card_component_animation } from 'src/app/core/components/shared/animations/card-component.animations';
 
 @Component({
   selector: 'app-violation-details-single',
   templateUrl: './violation-details-single.component.html',
-  styleUrls: ['./violation-details-single.component.scss']
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./violation-details-single.component.scss'],
+  animations: [card_component_animation('showHideCardBody')],
 })
 export class ViolationDetailsSingleComponent implements OnInit {
+  @Input() violationData: any;
+  public toggler: boolean[] = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  /**Function return id */
+  public identity(index: number, item: any): number {
+    return item.id;
   }
-
+  /**Function for toggle page in cards */
+  public toggleResizePage(value: number, indexName: string) {
+    this.toggler[value + indexName] = !this.toggler[value + indexName];
+  }
 }
