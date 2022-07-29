b5a65424171e0874fc5df76015e5efa1c6a0816c
Savke97 <savkeren97@gmail.com>
2022-07-29 12:39:57 +0200

    fix

--- src/app/core/components/applicant/state/pdf-export/application/step4-pdf/step4-pdf.component.ts
+++ src/app/core/components/applicant/state/pdf-export/application/step4-pdf/step4-pdf.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-step4-pdf',
   templateUrl: './step4-pdf.component.html',
-  styleUrls: ['./step4-pdf.component.scss']
+  /* styleUrls: ['./step4-pdf.component.scss'] */
 })
 export class Step4PdfComponent implements OnInit {
 
