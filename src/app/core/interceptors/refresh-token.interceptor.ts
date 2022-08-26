b21bf52e25fb31ca1fcde716baf2435b54856831
Marko Joker <djordjevicit97@gmail.com>
2022-08-26 17:08:54 +0200

    26.08 - update backend - implement refresh token

---
+++ src/app/core/interceptors/refresh-token.interceptor.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpRequest,
+  HttpHandler,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, catchError, throwError, switchMap } from 'rxjs';
+import { AccountService, SignInResponse } from 'appcoretruckassist';
+import { Router } from '@angular/router';
+import { UserLoggedService } from '../components/authentication/state/user-logged.service';
+
+@Injectable()
+export class RefreshTokenInterceptor implements HttpInterceptor {
+  constructor(private accountService: AccountService, private router: Router) {}
+
+  intercept(
+    httpRequest: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(httpRequest).pipe(
+      catchError((err: HttpErrorResponse) => {
+        const user: SignInResponse = JSON.parse(localStorage.getItem('user'));
+        if (err.status === 401 && user) {
+          console.log('Err status: ', err.status);
+          return this.accountService
+            .apiAccountRefreshPost({ refreshToken: user.refreshToken })
+            .pipe(
+              switchMap((res: any) => {
+                user.token = res.token;
+                user.refreshToken = res.refreshToken;
+                localStorage.setItem('user', JSON.stringify(user));
+                console.log('Refresh token: ', user);
+                return next.handle(httpRequest);
+              }),
+              catchError((err: HttpErrorResponse) => {
+                if (err.status === 404) {
+                  localStorage.removeItem('user');
+                  this.router.navigate(['/auth']);
+                }
+                return throwError(() => err);
+              })
+            );
+        }
+        return throwError(() => err);
+      })
+    );
+  }
+}
