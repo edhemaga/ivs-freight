7787bcbd2817b46053d5b793411cc5231c541216
Kvelail <talikadic@gmail.com>
2022-08-31 12:55:29 +0200

    search input

--- src/app/core/components/dashboard/dashboard.module.ts
+++ src/app/core/components/dashboard/dashboard.module.ts
@@ -1,8 +1,8 @@
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ReactiveFormsModule} from "@angular/forms";
-import {SharedModule} from "../shared/shared.module";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardPerformanceComponent } from './dashboard-performance/dashboard-performance.component';
 import { DashboardTopDriverComponent } from './dashboard-top-driver/dashboard-top-driver.component';
@@ -11,6 +11,8 @@ import { DashboardMapComponent } from './dashboard-map/dashboard-map.component';
 import { DashboardStateTrackingComponent } from './dashboard-state-tracking/dashboard-state-tracking.component';
 import { DashboardStateUsaComponent } from './dashboard-state-tracking/dashboard-state-usa/dashboard-state-usa.component';
 import { TaNoteModule } from '../shared/ta-note/ta-note.module';
+import { TruckassistSearchModule } from '../shared/truckassist-search/truckassist-search.module';
+
 @NgModule({
   declarations: [
     DashboardComponent,
@@ -19,16 +21,16 @@ import { TaNoteModule } from '../shared/ta-note/ta-note.module';
     DashboardPickupByStateComponent,
     DashboardMapComponent,
     DashboardStateTrackingComponent,
-    DashboardStateUsaComponent
+    DashboardStateUsaComponent,
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     ReactiveFormsModule,
     SharedModule,
-    TaNoteModule
+    TaNoteModule,
+    TruckassistSearchModule,
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class DashboardModule {
-}
+export class DashboardModule {}
