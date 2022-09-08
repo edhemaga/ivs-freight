393427c72ffe67c06ab22dc16b4d79211d03b118
Slavisa89 <61062720+Slavisa89@users.noreply.github.com>
2022-09-08 14:20:12 +0200

    Dispatch

--- src/app/core/components/dispatcher/dispatchboard-tables/dispatchboard-tables.component.ts
+++ src/app/core/components/dispatcher/dispatchboard-tables/dispatchboard-tables.component.ts
@@ -110,6 +110,15 @@ export class DispatchboardTablesComponent implements OnInit {
   checkForEmpty: string = "";
 
   @Input() set _dData(value) {
+    value.dispatches = value.dispatches.map(item => {
+      if( !item.hoursOfService ){
+        item.hoursOfService = {
+          hos: []
+        }
+      }
+      
+      return item;
+    })
     this.dData = value;
   }
 
@@ -137,6 +146,10 @@ export class DispatchboardTablesComponent implements OnInit {
       return item;
     });
   }
+
+  hosHelper = {
+    hos: [],
+  };
 
   __isBoardLocked: boolean = true;
 
@@ -391,5 +404,220 @@ export class DispatchboardTablesComponent implements OnInit {
       this.checkForEmpty = value;
       this.chd.detectChanges();
     }, 300);
+  }
+
+
+  toggleHos(tooltip: any, data: any, id: number) {
+  
+    this.hosHelper.hos = [];
+    if (data === null || data.hos.length === 0) {
+      data = {
+        hos: [
+          {
+            start: 0,
+            end: new Date().getHours() * 60 + new Date().getMinutes(),
+            flag: 'off',
+          },
+        ],
+      };
+      // this.gridData.forEach((element) => {
+      //   if (element.id === id) {
+      //     element.hosJson = data;
+      //   }
+      // });
+    }
+    if (tooltip.isOpen()) {
+      tooltip.close();
+    } else {
+      tooltip.open({ value: { data, id } });
+      setTimeout(() => {
+        data.hos.forEach((element, index) => {
+          const span = document.getElementById('valueSpan_' + index);
+          const cssStyle =
+            span.nextElementSibling.children[3].attributes[3].ownerElement;
+          const button = document.getElementById('buttonId_' + index);
+          button.style.marginLeft = data.hos[index].end / 3.8 + 'px';
+          span.style.width = cssStyle.clientWidth + 'px';
+          span.style.left = element.start / 3.8 + 'px';
+        });
+      });
+    }
+  }
+
+  returnListData(data: any, flag: string) {
+    const tempArr = [];
+    data.forEach((element) => {
+      if (element !== undefined && element.flag === flag) {
+        tempArr.push(element);
+      }
+    });
+    return tempArr;
+  }
+
+
+  dropHosList(event: any, data: any, id: number) {
+    const dragEl: any = event.previousContainer.data[event.previousIndex];
+    switch (data.length) {
+      case 1:
+        if (dragEl.flag === 'off') {
+          data[0].flag = 'on';
+        } else {
+          data[0].flag = 'off';
+        }
+        setTimeout(() => {
+          const span = document.getElementById('valueSpan_0');
+          const button = document.getElementById('buttonId_0');
+          const cssStyle =
+            span.nextElementSibling?.children[3].attributes[3].ownerElement;
+          span.style.width = cssStyle.clientWidth + 'px';
+          button.style.marginLeft = data[0].end / 3.8 + 'px';
+        });
+        break;
+      case 2:
+        if (event.previousContainer.id !== event.container.id) {
+          if (dragEl.flag === 'off') {
+            data = [
+              {
+                start: data[0].start,
+                end: data[1].end,
+                flag: 'on',
+              },
+            ];
+          } else {
+            data = [
+              {
+                start: data[0].start,
+                end: data[1].end,
+                flag: 'off',
+              },
+            ];
+          }
+          setTimeout(() => {
+            const span = document.getElementById('valueSpan_0');
+            const button = document.getElementById('buttonId_0');
+            const cssStyle =
+              span.nextElementSibling?.children[3].attributes[3].ownerElement;
+            span.style.width = cssStyle.clientWidth + 'px';
+            button.style.marginLeft = data[0].end / 3.8 + 'px';
+          });
+        }
+        break;
+      case 3:
+        if (event.previousContainer.id !== event.container.id) {
+          if (
+            event.previousIndex === 0 &&
+            event.previousContainer.data.length === 1
+          ) {
+            let tempObj = {};
+            if (
+              event.previousContainer.data[event.previousIndex].flag === 'off'
+            ) {
+              tempObj = {
+                start: event.container.data[0].start,
+                end: event.container.data[1].end,
+                flag: 'on',
+              };
+            } else {
+              tempObj = {
+                start: event.container.data[0].start,
+                end: event.container.data[1].end,
+                flag: 'off',
+              };
+            }
+            const tempArr = [];
+            tempArr.push(tempObj);
+            data = tempArr;
+            setTimeout(() => {
+              const span = document.getElementById('valueSpan_0');
+              const button = document.getElementById('buttonId_0');
+              const cssStyle =
+                span.nextElementSibling?.children[3].attributes[3].ownerElement;
+              span.style.width = cssStyle.clientWidth + 'px';
+              button.style.marginLeft = data[0].end / 3.8 + 'px';
+            });
+          } else if (event.previousContainer.data.length === 2) {
+            let tempObj = {};
+            const tempArr = [];
+            if (event.previousIndex === 0) {
+              if (event.previousContainer.data[0].flag === 'off') {
+                tempObj = {
+                  start: event.previousContainer.data[0].start,
+                  end: event.container.data[0].end,
+                  flag: 'on',
+                };
+              } else {
+                tempObj = {
+                  start: event.previousContainer.data[0].start,
+                  end: event.container.data[0].end,
+                  flag: 'off',
+                };
+              }
+              tempArr.push(tempObj);
+              tempArr.push(event.previousContainer.data[1]);
+              data = tempArr;
+              setTimeout(() => {
+                const span = document.getElementById('valueSpan_0');
+                const button = document.getElementById('buttonId_0');
+                const cssStyle =
+                  span.nextElementSibling?.children[3].attributes[3]
+                    .ownerElement;
+                span.style.width = cssStyle.clientWidth + 'px';
+                button.style.marginLeft = data[0].end / 3.8 + 'px';
+              });
+            } else if (event.previousIndex === 1) {
+              if (event.previousContainer.data[1].flag === 'off') {
+                tempObj = {
+                  start: event.container.data[0].start,
+                  end: event.previousContainer.data[1].end,
+                  flag: 'on',
+                };
+              } else {
+                tempObj = {
+                  start: event.container.data[0].start,
+                  end: event.previousContainer.data[1].end,
+                  flag: 'off',
+                };
+              }
+              tempArr.push(event.previousContainer.data[0]);
+              tempArr.push(tempObj);
+              data = tempArr;
+              setTimeout(() => {
+                const span = document.getElementById('valueSpan_1');
+                const button = document.getElementById('buttonId_1');
+                const cssStyle =
+                  span.nextElementSibling.children[3].attributes[3]
+                    .ownerElement;
+                span.style.width = cssStyle.clientWidth + 'px';
+                button.style.marginLeft = data[1].end / 3.8 + 'px';
+                span.style.left = data[1].start / 3.8 + 'px';
+              });
+            }
+          }
+        }
+    }
+    // this.gridData.forEach((element) => {
+    //   if (element.id === id) {
+    //     element.hosJson.hos = data;
+    //   }
+    // });
+  }
+
+  returnValueId(i) {
+    console.log("HOS RETURN VALUE ID");
+    return 'valueSpan_' + i;
+  }
+
+  formatTime(minValue, maxValue) {
+    console.log("HOS RETURN VALUE formatTime");
+    const minutes = maxValue - minValue;
+    const m = minutes % 60;
+    const h = (minutes - m) / 60;
+    const suffix = h >= 12 ? 'PM' : 'AM';
+    const formatedH = h > 12 ? h - 12 : h;
+    return h.toString() + ':' + (m < 10 ? '0' : '') + m.toString();
+  }
+
+  returnButtonId(i) {
+    return 'buttonId_' + i;
   }
 }
