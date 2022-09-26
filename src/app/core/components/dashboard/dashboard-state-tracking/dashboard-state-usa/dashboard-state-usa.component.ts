82aa3e08df0abd6d150868d30339f8d110d0030d
Andrej Nikolic <andrejnikolic999@gmail.com>
2022-09-26 11:33:44 +0200

    Dashboard state hover animation

--- src/app/core/components/dashboard/dashboard-state-tracking/dashboard-state-usa/dashboard-state-usa.component.ts
+++ src/app/core/components/dashboard/dashboard-state-tracking/dashboard-state-usa/dashboard-state-usa.component.ts
@@ -1,36 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-state-usa',
   templateUrl: './dashboard-state-usa.component.html',
-  styleUrls: ['./dashboard-state-usa.component.scss']
+  styleUrls: ['./dashboard-state-usa.component.scss'],
 })
 export class DashboardStateUsaComponent implements OnInit {
   @Input() statesColor: any;
   hoveredItem: any;
-  hoveredContry: string = "";
-  hoveredContryLetter: string = "";
-  currentCountry: string = "";
+  hoveredContry: string = '';
+  hoveredContryLetter: string = '';
+  currentCountry: string = '';
 
-  constructor() {
-  }
+  constructor(private ref: ChangeDetectorRef) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   setPopoverData(country: string, countryLetters?: string): void {
     this.currentCountry = country;
     this.hoveredItem = this.statesColor[country];
     this.hoveredContry = `#${country}`;
     this.hoveredContryLetter = `#${countryLetters}`;
+    this.ref.detectChanges();
   }
 
-  removePopoverData(){
-    this.hoveredItem = null
-    return false;
-    this.currentCountry = '';
-    this.hoveredContry = '';
-    this.hoveredContryLetter = '';
+  removePopoverData() {
+    this.hoveredItem = null;
+    this.ref.detectChanges();
   }
-
 }
