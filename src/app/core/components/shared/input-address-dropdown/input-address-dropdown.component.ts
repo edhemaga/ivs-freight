b2144d839ed7b004dfd345e639edb51d4507f4df
Andrej Nikolic <andrejnikolic999@gmail.com>
2022-10-18 14:31:46 +0200

    routing address keyboad commands

--- src/app/core/components/shared/input-address-dropdown/input-address-dropdown.component.ts
+++ src/app/core/components/shared/input-address-dropdown/input-address-dropdown.component.ts
@@ -9,6 +9,7 @@ import {
   ViewChild,
   ViewEncapsulation,
   ChangeDetectorRef,
+  HostListener,
 } from '@angular/core';
 import { filter, Subject, switchMap, takeUntil } from 'rxjs';
 import { AddressService } from 'src/app/core/services/shared/address.service';
@@ -75,6 +76,20 @@ export class InputAddressDropdownComponent
 
   @Output() incorrectEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: any) {
+    const key = event.keyCode;
+    if (this.inputConfig.name == 'RoutingAddress') {
+      if (key === 13) {
+        if (this.currentAddressData) {
+          this.onCommands(event, 'confirm');
+        }
+      } else if (key === 27) {
+        this.clearInput(event);
+      }
+    }
+  }
+
   constructor(
     @Self() public superControl: NgControl,
     private addressService: AddressService,
@@ -101,6 +116,11 @@ export class InputAddressDropdownComponent
         filter((term: string) => {
           if (!term) {
             this.addresList = [];
+          } else if (
+            term != this.currentAddressData?.address.address &&
+            this.inputConfig.name == 'RoutingAddress'
+          ) {
+            this.currentAddressData = null;
           }
           if (
             this.inputConfig.name != 'RoutingAddress' &&
@@ -176,13 +196,8 @@ export class InputAddressDropdownComponent
         this.commandEvent.emit(this.currentAddressData);
       }
 
-      this.currentAddressData = null;
-      this.addressExpanded = false;
-      this.addresList = [];
-      this.getSuperControl.setValue(null);
-      this.activeAddress = null;
-      this.inputDropdown?.inputRef?.clearInput(e);
-      this.chosenFromDropdown = false;
+      this.closeAddress();
+      this.clearInput(e);
     }
   }
 
@@ -190,6 +205,19 @@ export class InputAddressDropdownComponent
     if (!this.addressExpanded) {
       this.addressExpanded = true;
     }
+  }
+
+  closeAddress() {
+    this.addressExpanded = false;
+  }
+
+  clearInput(e) {
+    this.currentAddressData = null;
+    this.addresList = [];
+    this.getSuperControl.setValue(null);
+    this.activeAddress = null;
+    this.inputDropdown?.inputRef?.clearInput(e);
+    this.chosenFromDropdown = false;
   }
 
   checkSearchLayers(value) {
