c5f4b98e711cd1a9c4e06b5d521c57cd6db46550
Andrej Nikolic <andrejnikolic999@gmail.com>
2022-11-13 23:18:53 +0100

    upload ui fix

--- src/app/core/components/shared/ta-upload-files/ta-upload-file/ta-upload-file.component.ts
+++ src/app/core/components/shared/ta-upload-files/ta-upload-file/ta-upload-file.component.ts
@@ -107,8 +107,8 @@ export class TaUploadFileComponent implements OnInit, OnDestroy {
   public afterLoadComplete(pdf: PDFDocumentProxy) {
     this.numberOfFilePages =
       pdf._pdfInfo.numPages === 1
-        ? pdf._pdfInfo.numPages.toString().concat(' ', 'PAGE')
-        : pdf._pdfInfo.numPages.toString().concat(' ', 'PAGES');
+        ? pdf._pdfInfo.numPages.toString().concat(' ', 'page')
+        : pdf._pdfInfo.numPages.toString().concat(' ', 'pages');
   }
 
   public onAction(action: string) {
