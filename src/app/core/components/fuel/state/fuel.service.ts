5c05cf32987312e042f73e5c9724ec473514ee38
Savke97 <savkeren97@gmail.com>
2022-11-25 11:45:46 +0100

    husky fix

--- src/app/core/components/fuel/state/fuel.service.ts
+++ src/app/core/components/fuel/state/fuel.service.ts
@@ -1,4 +1,4 @@
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { FuelStopResponse } from './../../../../../../appcoretruckassist/model/fuelStopResponse';
 import { Injectable } from '@angular/core';
 import {
