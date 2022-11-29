801e7309373114e8351b7382e43200f02fe3b8df
Slavisa89 <61062720+Slavisa89@users.noreply.github.com>
2022-11-29 12:15:54 +0100

    app rename

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/truckassist-fe'),
+      dir: require('path').join(__dirname, './coverage/carrierassist-fe'),
       subdir: '.',
       reporters: [
         { type: 'html' },
