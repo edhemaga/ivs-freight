9b56420adf6bb3a3e0b9b3bf9a6eb7358f15de6d
Slavisa89 <61062720+Slavisa89@users.noreply.github.com>
2022-12-08 15:57:41 +0100

    Dispatchboard new design

--- src/app/core/components/dispatch/dispatch-table/dispatch-table.component.ts
+++ src/app/core/components/dispatch/dispatch-table/dispatch-table.component.ts
@@ -1,7 +1,21 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    Input,
+    OnInit,
+    ViewEncapsulation,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
+import {
+    CreateDispatchCommand,
+    DispatchStatus,
+    UpdateDispatchCommand,
+} from 'appcoretruckassist';
+import { catchError, of } from 'rxjs';
 import { ColorFinderPipe } from '../../dispatcher/pipes/color-finder.pipe';
 import { DispatchBoardLocalResponse } from '../../dispatcher/state/dispatcher.model';
+import { DispatcherStoreService } from '../../dispatcher/state/dispatcher.service';
 import { TaInputService } from '../../shared/ta-input/ta-input.service';
 
 @Component({
@@ -9,9 +23,11 @@ import { TaInputService } from '../../shared/ta-input/ta-input.service';
     templateUrl: './dispatch-table.component.html',
     styleUrls: ['./dispatch-table.component.scss'],
     encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [ColorFinderPipe],
 })
 export class DispatchTableComponent implements OnInit {
+    checkForEmpty: string = '';
     dData: DispatchBoardLocalResponse = {};
     truckFormControll: FormControl = new FormControl();
     truckList: any[];
@@ -48,6 +64,7 @@ export class DispatchTableComponent implements OnInit {
 
     @Input() set _dData(value) {
         this.dData = JSON.parse(JSON.stringify(value));
+        console.log(this.dData);
     }
 
     @Input() dDataIndx: number;
@@ -57,45 +74,22 @@ export class DispatchTableComponent implements OnInit {
     openedTruckDropdown: number = -1;
     openedTrailerDropdown: number = -1;
     openedDriverDropdown: number = -1;
-
-    data: any[] = [
-        {
-            add_new_truck: true,
-            add_new_driver: true,
-        },
-        {
-            add_new_trailer: true,
-            active_new_truck: true,
-        },
-        {
-            add_new_truck: true,
-            add_new_trailer: true,
-        },
-        {
-            add_new_truck: true,
-        },
-        {
-            add_new_trailer: true,
-            add_new_driver: true,
-        },
-        {
-            add_new_truck: true,
-        },
-        {
-            add_new_trailer: true,
-            add_new_driver: true,
-        },
-    ];
+    statusOpenedIndex: number = -1;
+    showAddAddressField: number = -1;
+    savedTruckId: any;
 
     __isBoardLocked: boolean = true;
-
-    selectTruck: FormControl = new FormControl(null);
+    __change_in_proggress: boolean = false;
 
     @Input() set isBoardLocked(isLocked: boolean) {
         this.__isBoardLocked = isLocked;
     }
 
-    constructor(private colorPipe: ColorFinderPipe) {}
+    constructor(
+        private dss: DispatcherStoreService,
+        private chd: ChangeDetectorRef,
+        private colorPipe: ColorFinderPipe
+    ) {}
 
     ngOnInit(): void {}
 
@@ -115,9 +109,9 @@ export class DispatchTableComponent implements OnInit {
             case 'trailer':
                 this.openedTrailerDropdown = -1;
                 break;
-            case 'driver': 
+            case 'driver':
                 this.openedDriverDropdown = -1;
-            break;
+                break;
         }
 
         this.truckFormControll.reset();
@@ -133,5 +127,161 @@ export class DispatchTableComponent implements OnInit {
 
     showDriverDropdown(ind: number) {
         this.openedDriverDropdown = ind;
+    }
+
+    addTruck(e) {
+        if (e) {
+            if (this.openedTruckDropdown == -2) this.savedTruckId = e;
+            else this.dData.dispatches[this.openedTruckDropdown].truck = e;
+            this.showAddAddressField = this.openedTruckDropdown;
+        }
+
+        this.openedTruckDropdown = -1;
+    }
+
+    addTrailer(e) {
+        if (e) {
+            this.updateOrAddDispatchBoardAndSend(
+                'trailerId',
+                e.id,
+                this.openedTrailerDropdown
+            );
+        }
+
+        this.openedTrailerDropdown = -1;
+    }
+
+    handleInputSelect(e: any) {
+        if (e.valid) {
+            this.updateOrAddDispatchBoardAndSend(
+                'location',
+                e.address,
+                this.showAddAddressField
+            );
+        }
+    }
+
+    onHideDropdown(e) {
+        setTimeout(() => {
+            if (this.showAddAddressField != -2)
+                this.dData.dispatches[this.showAddAddressField].truck = null;
+            this.showAddAddressField = -1;
+            this.chd.detectChanges();
+        }, 200);
+    }
+
+    addDriver(e) {
+        if (e) {
+            const driverOrCoDriver = !this.dData.dispatches[
+                this.openedDriverDropdown
+            ]?.driver
+                ? 'driverId'
+                : 'coDriverId';
+            this.updateOrAddDispatchBoardAndSend(
+                driverOrCoDriver,
+                e.id,
+                this.openedDriverDropdown
+            );
+        }
+
+        this.openedDriverDropdown = -1;
+    }
+
+    addStatus(e) {
+        if (e) {
+            if ([7, 8, 9, 4, 10, 6].includes(e.statusValue.id)) {
+                this.dData.dispatches[this.statusOpenedIndex].status = e;
+                this.showAddAddressField = this.statusOpenedIndex;
+            } else {
+                this.updateOrAddDispatchBoardAndSend(
+                    'status',
+                    e.statusValue.name,
+                    this.statusOpenedIndex
+                );
+            }
+        }
+
+        this.statusOpenedIndex = -1;
+    }
+
+    updateOrAddDispatchBoardAndSend(key, value, index) {
+        const oldData = this.dData.dispatches[index]
+            ? JSON.parse(JSON.stringify(this.dData.dispatches[index]))
+            : {};
+
+        //const dataId = oldData.id;
+        let oldUpdateData: CreateDispatchCommand | UpdateDispatchCommand = {
+            status: oldData.status
+                ? (oldData.status?.statusValue.name as DispatchStatus)
+                : 'Off',
+            order: oldData.order,
+            truckId: oldData.truck ? oldData.truck?.id : null,
+            trailerId: oldData.trailer ? oldData.trailer?.id : null,
+            driverId: oldData.driver ? oldData.driver?.id : null,
+            coDriverId: oldData.coDriver ? oldData.coDriver?.id : null,
+            location: oldData.location?.address ? oldData.location : null,
+            // hourOfService: 0,
+            note: oldData.note,
+            loadIds: [],
+        };
+
+        let newData: any = {
+            ...oldUpdateData,
+            [key]: value,
+        };
+
+        this.__change_in_proggress = true;
+
+        this.checkForEmpty = key;
+
+        if (oldData.id) {
+            newData = {
+                id: oldData.id,
+                ...newData,
+            };
+
+            if (!value && key == 'truckId') newData.location = null;
+
+            this.dss
+                .updateDispatchBoard(newData, this.dData.id)
+                .pipe(
+                    catchError((error) => {
+                        this.checkEmptySet = '';
+                        return of([]);
+                    })
+                )
+                .subscribe((data) => {
+                    this.dss.updateCountList(this.dData.id, key, value);
+                    this.dss.updateModalList();
+                    this.checkEmptySet = '';
+                    this.__change_in_proggress = false;
+                });
+        } else {
+            newData.dispatchBoardId = this.dData.id;
+
+            if (key == 'location') newData.truckId = this.savedTruckId.id;
+
+            this.dss
+                .createDispatchBoard(newData, this.dData.id)
+                .pipe(
+                    catchError((error) => {
+                        this.checkEmptySet = '';
+                        return of([]);
+                    })
+                )
+                .subscribe((data) => {
+                    this.checkEmptySet = '';
+                    this.dss.updateCountList(this.dData.id, key, value);
+                    this.dss.updateModalList();
+                    this.__change_in_proggress = false;
+                });
+        }
+    }
+
+    set checkEmptySet(value) {
+        setTimeout(() => {
+            this.checkForEmpty = value;
+            this.chd.detectChanges();
+        }, 300);
     }
 }
