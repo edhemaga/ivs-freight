5cebaf63e0e24b811cac73d51ca4c89cfa809e2e
Andrej Nikolic <andrejnikolic999@gmail.com>
2022-12-15 14:20:28 +0100

    company documents

--- src/app/core/components/settings/settings-document/settings-document.module.ts
+++ src/app/core/components/settings/settings-document/settings-document.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SettingsDocumentComponent } from './settings-document.component';
 import { SettingsDocumentRoutes } from './settings-document.routing';
 import { SharedModule } from '../../shared/shared.module';
+import { TruckassistSearchModule } from '../../shared/truckassist-search/truckassist-search.module';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    imports: [CommonModule, SettingsDocumentRoutes, SharedModule],
+    imports: [CommonModule, SettingsDocumentRoutes, SharedModule, TruckassistSearchModule, NgbTooltipModule],
     declarations: [SettingsDocumentComponent],
 })
 export class SettingsDocumentModule {}
