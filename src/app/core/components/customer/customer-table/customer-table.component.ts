c5a46e7c73a8a0905e056fb741ff610d9f4a39ff
Savke97 <savkeren97@gmail.com>
2022-12-23 13:59:18 +0100

    Customer broker columns

--- src/app/core/components/customer/customer-table/customer-table.component.ts
+++ src/app/core/components/customer/customer-table/customer-table.component.ts
@@ -437,31 +437,34 @@ export class CustomerTableComponent
         return {
             ...data,
             isSelected: false,
-            textInvAgeing: {
+            tableAddressPhysical: 'Nije Povezano',
+            tableAddressBilling: 'Mije Povezano',
+            tablePaymentDetailAvailCredit: 'Nije Povezano',
+            tablePaymentDetailCreditLimit: 'Nije Povezano',
+            tablePaymentDetailTerm: 'Nije Povezano',
+            tablePaymentDetailDTP: 'Nije Povezano',
+            tablePaymentDetailInvAgeing: {
                 bfb: 0,
                 dnu: 0,
                 amount: 'Nije Povezano',
             },
-            textContact: data?.brokerContacts?.length
-                ? data.brokerContacts.length
-                : 0,
-            textAddress: data?.mainAddress
-                ? data.mainAddress.city + ', ' + data.mainAddress.state
-                : '',
-            loadCount: data?.loadCount
+            tableLoads: data?.loadCount
                 ? this.thousandSeparator.transform(data.loadCount)
                 : '',
-            textTotal: data?.total
-                ? '$' + this.thousandSeparator.transform(data.total)
-                : '',
-            textUnpaid: 'Nije Povezano',
-            textOnetoTwentyDays: 'Nije Povezano',
-            raiting: {
+            tableMiles: 'Nije Povezano',
+            tablePPM: 'Nije Povezano',
+            tableRevenue: 'Nije Povezano',
+            tableRaiting: {
                 hasLiked: data.currentCompanyUserRating === 1,
                 hasDislike: data.currentCompanyUserRating === -1,
                 likeCount: data?.upCount ? data.upCount : '0',
                 dislikeCount: data?.downCount ? data.downCount : '0',
             },
+            tableContact: data?.brokerContacts?.length
+                ? data.brokerContacts.length
+                : 0,
+            tableAdded: 'Nije Povezano',
+            tableEdited: 'Nije Povezano',
         };
     }
 
