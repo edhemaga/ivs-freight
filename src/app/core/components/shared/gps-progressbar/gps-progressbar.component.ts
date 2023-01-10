e9174b9e8c6e2c236f58608bcc7a4dc673711fc1
Andrej Nikolic <andrejnikolic999@gmail.com>
2023-01-10 12:25:00 +0100

    dispatch gps progress

--- src/app/core/components/shared/gps-progressbar/gps-progressbar.component.ts
+++ src/app/core/components/shared/gps-progressbar/gps-progressbar.component.ts
@@ -9,29 +9,51 @@ export class GpsProgressbarComponent implements OnInit {
     constructor() {}
 
     currentPosition: number = 50;
+    currentStopHovered: boolean = false;
+    currentStop: any = {
+        type: 'currentStop',
+        position: 50,
+        location: 'Philadelphia, PA'
+    };
+    gpsTitle: string = '128.4 mi';
 
     gpsProgress: any = [
         {
             type: 'start',
             position: 0,
+            location: 'Overland Park, KS',
         },
         {
             type: 'pickup',
             position: 10,
+            location: 'Chicago, IL',
         },
         {
             type: 'pickup',
             position: 40,
+            location: 'Los Angeles, CA',
         },
         {
             type: 'delivery',
             position: 60,
+            location: 'New York, NY',
         },
         {
             type: 'delivery',
             position: 100,
+            location: 'Miami, FL',
         },
     ];
 
     ngOnInit(): void {}
+
+    hoverStop(stop: any) {
+        this.gpsTitle = stop.location;
+        this.currentStopHovered = stop.type == 'currentStop' ? true : false;
+    }
+
+    leaveStop() {
+        this.gpsTitle = '128.4 mi';
+        this.currentStopHovered = false;
+    }
 }
