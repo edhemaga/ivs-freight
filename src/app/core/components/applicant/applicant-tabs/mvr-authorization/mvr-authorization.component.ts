7595d2fb67c0dc6a1095f3000da06efa8f114345
Kvelail <talikadic@gmail.com>
2023-01-17 15:47:34 +0100

    selectedmode

--- src/app/core/components/applicant/applicant-tabs/mvr-authorization/mvr-authorization.component.ts
+++ src/app/core/components/applicant/applicant-tabs/mvr-authorization/mvr-authorization.component.ts
@@ -32,7 +32,7 @@ import { SelectedMode } from '../../state/enum/selected-mode.enum';
 export class MvrAuthorizationComponent implements OnInit, OnDestroy {
     private destroy$ = new Subject<void>();
 
-    public selectedMode: string = SelectedMode.REVIEW;
+    public selectedMode: string = SelectedMode.APPLICANT;
 
     public isValidLoad: boolean;
 
