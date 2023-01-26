13233d6bc36b85a3bc6010e8f807cf7d5695621d
MarkoJoker <djordjevicit97@gmail.com>
2023-01-26 17:58:57 +0100

    Commit - javila se greska za additionalBillingTypes opet , kako ??? (load) - crvena kartica tekst u drugi red stavi opacity 70% (LOAD) - hvale indikatori za vreme na kartici (AM - PM LOAD) - focus kartice (ispraviti padding i margine - load modal)

--- src/app/core/components/modals/load-modal/pipes/load-datetime-range.pipe.ts
+++ src/app/core/components/modals/load-modal/pipes/load-datetime-range.pipe.ts
@@ -6,6 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LoadDatetimeRangePipe implements PipeTransform {
     transform(value: string, secondValue: string): any {
         if (secondValue) {
+            console.log('value - second: ', value, secondValue);
             return `${value} - ${secondValue}`;
         }
         return value;
