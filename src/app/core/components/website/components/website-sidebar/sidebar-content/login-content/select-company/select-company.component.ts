6a7c9c4147b44476bcacbd848fc4db994b66f2f7
Kvelail <talikadic@gmail.com>
2023-02-09 17:03:20 +0100

    select company fix

--- src/app/core/components/website/components/website-sidebar/sidebar-content/login-content/select-company/select-company.component.ts
+++ src/app/core/components/website/components/website-sidebar/sidebar-content/login-content/select-company/select-company.component.ts
@@ -1,10 +1,8 @@
 import { DOCUMENT } from '@angular/common';
-
 import {
     Component,
     EventEmitter,
     Inject,
-    Input,
     OnDestroy,
     OnInit,
     Output,
@@ -14,6 +12,10 @@ import { Router } from '@angular/router';
 import { UntypedFormBuilder } from '@angular/forms';
 
 import { Subject, tap } from 'rxjs';
+
+import moment from 'moment';
+
+import { convertTimeFromBackend } from 'src/app/core/utils/methods.calculations';
 
 import { WebsiteAuthService } from 'src/app/core/components/website/state/service/website-auth.service';
 
@@ -26,47 +28,126 @@ import { SelectCompanyResponse, SignInResponse } from 'appcoretruckassist';
     encapsulation: ViewEncapsulation.None,
 })
 export class SelectCompanyComponent implements OnInit, OnDestroy {
-    @Input() userData: SignInResponse;
-    @Input() lastLoginInCompany: number;
-    
     @Output() goBackToLogin = new EventEmitter<boolean>();
 
     private destroy$ = new Subject<void>();
 
-    public saveCompany;
+    public saveCompany: any;
     public dotsTrue: boolean;
     public slideConfig: any;
+    public userData: SignInResponse;
+    public lastLoginInCompany: any;
+    public dates: any = [];
+    public newUser: any;
+    public id: any;
 
     constructor(
-        @Inject(DOCUMENT) private document: HTMLDocument,
+        @Inject(DOCUMENT) private document: Document,
         private router: Router,
         private formBuilder: UntypedFormBuilder,
         private websiteAuthService: WebsiteAuthService
     ) {}
 
-    ngOnInit(): void {
+    ngOnInit() {
         this.createForm();
 
-        this.userData = JSON.parse(localStorage.getItem('user'));
+        this.user(JSON.parse(localStorage.getItem('user')));
 
         this.userData.companies.length < 5
             ? (this.dotsTrue = false)
             : (this.dotsTrue = true);
-
-        this.slideConfig = {
-            centerMode: true,
-            infinite: false,
-            slidesToScroll: 1,
-            dots: this.dotsTrue,
-            arrows: true,
-            variableWidth: true,
-        };
     }
 
     private createForm(): void {
         this.saveCompany = this.formBuilder.group({
             savedCompany: true,
         });
+    }
+
+    public user(data): void {
+        this.userData = data;
+
+        data.companies.forEach((res) => {
+            this.lastLoginInCompany = this.calculateDiff(
+                convertTimeFromBackend(res.lastLogin)
+            );
+
+            this.dates.push(
+                moment.utc(res.lastLogin).local().format('MM/DD/YY HH:mm:ss')
+            );
+        });
+
+        let createNewObject = {
+            ...this.userData,
+            companies: this.userData.companies.map((item) => {
+                return {
+                    ...item,
+                    lastLogin: moment
+                        .utc(item.lastLogin)
+                        .local()
+                        .format('MM/DD/YY HH:mm:ss'),
+                };
+            }),
+        };
+
+        this.newUser = {
+            ...createNewObject,
+            companies: createNewObject.companies.map((item) => {
+                return {
+                    ...item,
+                    LastActiveCompany:
+                        item.lastLogin == this.getNewerDate(this.dates) &&
+                        (this.id = item.id),
+                };
+            }),
+        };
+
+        //Slick Carousel Config
+        this.slideConfig = {
+            infinite: false,
+            slidesToScroll: 1,
+            dots: this.dotsTrue,
+            arrows: true,
+            variableWidth: true,
+            focusOnSelect: true,
+            centerMode: true,
+            initialSlide: this?.id - 1,
+        };
+    }
+
+    public getNewerDate(dates: string[]) {
+        let newestDate = moment.utc(dates[0], 'MM/DD/YY HH:mm:ss');
+
+        for (let i = 1; i < dates.length; i++) {
+            const currentDate = moment.utc(dates[i], 'MM/DD/YY HH:mm:ss');
+
+            if (currentDate.isAfter(newestDate)) {
+                newestDate = currentDate;
+            }
+        }
+
+        return newestDate.format('MM/DD/YY HH:mm:ss');
+    }
+
+    //Calculate last login in company to display
+    public calculateDiff(dateSent) {
+        let currentDate = new Date();
+
+        dateSent = new Date(dateSent);
+
+        return Math.floor(
+            (Date.UTC(
+                currentDate.getFullYear(),
+                currentDate.getMonth(),
+                currentDate.getDate()
+            ) -
+                Date.UTC(
+                    dateSent.getFullYear(),
+                    dateSent.getMonth(),
+                    dateSent.getDate()
+                )) /
+                (1000 * 60 * 60 * 24)
+        );
     }
 
     public goToLogin(): void {
