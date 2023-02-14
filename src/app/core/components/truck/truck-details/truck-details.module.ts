fa057005898ddba2b208df4f431e6fbc94eba96e
Slavisa89 <61062720+Slavisa89@users.noreply.github.com>
2023-02-14 17:11:44 +0100

    Trailer and driver details fix

--- src/app/core/components/truck/truck-details/truck-details.module.ts
+++ src/app/core/components/truck/truck-details/truck-details.module.ts
@@ -6,6 +6,17 @@ import { TruckDetailsRoutes } from './truck-details.routing';
 import { TruckDetailsCardComponent } from '../truck-details-card/truck-details-card.component';
 import { SharedModule } from '../../shared/shared.module';
 import { formatDatePipe } from 'src/app/core/pipes/formatDate.pipe';
+import { ProfileImagesComponent } from '../../shared/profile-images/profile-images.component';
+import { TaCopyComponent } from '../../shared/ta-copy/ta-copy.component';
+import { TaCustomCardComponent } from '../../shared/ta-custom-card/ta-custom-card.component';
+import { TaUploadFilesComponent } from '../../shared/ta-upload-files/ta-upload-files.component';
+import { TaInputNoteComponent } from '../../shared/ta-input-note/ta-input-note.component';
+import { TaReCardComponent } from '../../shared/ta-common-card/ta-re-card.component';
+import { TruckassistProgressExpirationComponent } from '../../shared/truckassist-progress-expiration/truckassist-progress-expiration.component';
+import { TaCounterComponent } from '../../shared/ta-counter/ta-counter.component';
+import { TaCommonHeaderComponent } from '../../shared/ta-details-header/ta-details-header.component';
+import { TaDetailsHeaderCardComponent } from '../../shared/ta-details-header-card/ta-details-header-card.component';
+import { TaChartComponent } from '../../standalone-components/ta-chart/ta-chart.component';
 
 @NgModule({
     declarations: [
@@ -18,7 +29,19 @@ import { formatDatePipe } from 'src/app/core/pipes/formatDate.pipe';
         CommonModule,
         TruckDetailsRoutes,
         SharedModule,
-        formatDatePipe
+        formatDatePipe,
+        ProfileImagesComponent,
+        TaCopyComponent,
+        TaCustomCardComponent,
+        TaUploadFilesComponent,
+        TaInputNoteComponent,
+        TaReCardComponent,
+        TruckassistProgressExpirationComponent,
+        TaCounterComponent,
+        formatDatePipe,
+        TaCommonHeaderComponent,
+        TaDetailsHeaderCardComponent,
+        TaChartComponent
     ],
 })
 export class TruckDetailsModule {}
