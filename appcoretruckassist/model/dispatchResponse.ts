0cbfcf221d55dffbfaa7a86517218d4fa6a6d7da
Slavisa89 <61062720+Slavisa89@users.noreply.github.com>
2023-02-23 14:15:32 +0100

    backend update

--- appcoretruckassist/model/dispatchResponse.ts
+++ appcoretruckassist/model/dispatchResponse.ts
@@ -37,9 +37,11 @@ export interface DispatchResponse {
     lastStatusDateTime?: string;
     currentStopType?: EnumValue;
     nextStopType?: EnumValue;
+    possibleNextStatuses?: Array<DispatchStatusResponse> | null;
     hoursOfService?: Array<DispatchHosResponse> | null;
     note?: string | null;
     activeLoad?: LoadShortResponse;
+    assignedLoads?: Array<LoadShortResponse> | null;
     parkingSlot?: ParkingSlotShortResponse;
 }
 
