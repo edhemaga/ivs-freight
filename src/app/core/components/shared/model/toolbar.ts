8d07aa1d91ded81cfcde11e3c5c8b119ceb631ea
Nikola Narancic <nikola.narancic95@gmail.com>
2024-01-26 13:00:12 +0100

    Fix for toolbar

---
+++ src/app/core/components/shared/model/toolbar.ts
@@ -0,0 +1,12 @@
+export interface optionsPopupContent {
+    text: string;
+    svgPath: string;
+    backToListIcon?: string;
+    active: boolean;
+    hide: boolean;
+    showBackToList?: boolean;
+    acthasOwnSubOpionsive?: boolean;
+    hasTopBorder?: boolean;
+    hasOwnSubOpions?: boolean;
+    isInactive?: boolean;
+}
