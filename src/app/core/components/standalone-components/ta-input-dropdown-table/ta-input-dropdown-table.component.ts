ab9d06cef5c7508b841279c51bf279bd738f99ef
vladimir9961 <vladimir.mil21@gmail.com>
2024-01-29 16:29:32 +0100

    ta-input-dropdown update for owner

--- src/app/core/components/standalone-components/ta-input-dropdown-table/ta-input-dropdown-table.component.ts
+++ src/app/core/components/standalone-components/ta-input-dropdown-table/ta-input-dropdown-table.component.ts
@@ -4,6 +4,11 @@ import {
     NgbPopoverModule,
     NgbTooltip,
 } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// animations
+import { DropdownData } from '../../../model/input-dropdown.model';
 
 // modules
 import { CommonModule } from '@angular/common';
@@ -11,7 +16,9 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 
 // models
 import { CardDetails } from '../../shared/model/cardTableData';
-import { DropdownData } from '../../../model/input-dropdown.model';
+
+// services
+import { DetailsDataService } from 'src/app/core/services/details-data/details-data.service';
 
 @Component({
     selector: 'app-ta-input-dropdown-table',
@@ -22,6 +29,7 @@ import { DropdownData } from '../../../model/input-dropdown.model';
         AngularSvgIconModule,
         NgbModule,
         NgbPopoverModule,
+        FormsModule,
     ],
     templateUrl: './ta-input-dropdown-table.component.html',
     styleUrls: ['./ta-input-dropdown-table.component.scss'],
@@ -32,9 +40,33 @@ export class TaInputDropdownTableComponent implements OnInit {
     public tooltip: NgbTooltip;
     public dropDownActive: number;
 
-    constructor() {}
+    public truckDropdown: boolean = true;
+    public trailerDropdown: boolean = true;
+
+    searchTerm: string = '';
+    arrayToFilter: string[] = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+    filteredArray: string[] = [];
+    constructor(
+        private router: Router,
+        private detailsDataService: DetailsDataService
+    ) {}
 
     ngOnInit(): void {}
+
+    public filterArray(event: InputEvent): void {
+        // if (event.target instanceof HTMLInputElement) {
+        //     console.log(this.data.filter((item) =>
+        //     item.toLowerCase().includes(this.searchTerm.toLowerCase())
+        //   ));
+        //     // Now you can use event.target.value safely
+        // }
+    }
+
+    public goToDetailsPage(card: CardDetails, link: string): void {
+        this.detailsDataService.setNewData(card);
+
+        this.router.navigate([link]);
+    }
 
     // Owner dropdown
     public toggleDropdownOwnerFleet(
@@ -47,5 +79,22 @@ export class TaInputDropdownTableComponent implements OnInit {
         tooltip.open({ data: card });
 
         return;
+    }
+
+    public toggleDropdownTruckTrailer(
+        truckOrTrailer: string,
+        toggle: boolean
+    ): void {
+        switch (truckOrTrailer) {
+            case 'Truck':
+                this.truckDropdown = toggle;
+                break;
+
+            case 'Trailer':
+                this.trailerDropdown = toggle;
+                break;
+            default:
+                break;
+        }
     }
 }
