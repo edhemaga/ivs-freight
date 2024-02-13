881d40f3feae76b4373ff4699b36edf31286f161
vladimir9961 <vladimir.mil21@gmail.com>
2024-02-13 16:15:53 +0100

    company settings bug fixes

---
+++ src/app/core/components/settings/settings-company/utils/settings.enum.ts
@@ -0,0 +1,10 @@
+export enum SETINGS_ENUMS {
+    NOWRRAP = 'nowrap',
+    PRE_LINE = 'pre-line',
+
+    SIXTY = '60px',
+    FOURTY_EIGHT = '48px',
+    THIRTY_TWO = '32px',
+    TWENTY_THREE = '23px',
+    TWENTY = '20px',
+}
