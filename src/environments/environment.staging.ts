01b805a95fd2f8457755d863a244fdacc153caae
Slavisa Markovic <sslavishaa@gmail.com>
2024-02-21 14:02:03 +0100

    Autocompleate key added to all envs

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,7 @@
 export const environment = {
     API_ENDPOINT: 'https://api-stage.carriera.io',
     GPS_ENDPOINT: 'https://api-stage.carriera.io',
+    GOOGLE_AUTOCOMPLETE_KEY: 'f72ad2c9-d4a6-46c5-9093-7085d43cf6b2',
     staging: true,
     production: false,
     perPage: 1000,
