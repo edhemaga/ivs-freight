4515372b39160058a7c234b0fcfb7f843cc6893f
anndrej7 <andrejnikolic999@gmail.com>
2024-03-14 12:01:10 +0100

    fixes

--- src/app/core/components/driver/driver-details-card/state/enums/driver-enums.ts
+++ src/app/core/components/driver/driver-details-card/state/enums/driver-enums.ts
@@ -14,9 +14,28 @@ export enum DriverEnum {
     NEW_MEDICAL = 'new-medical',
     NEW_MVR = 'new-mvr',
     TEST = 'Test',
-    DELETE = 'Delete'
+    DELETE = 'Delete',
+    EDIT = 'Edit',
+    RENEW = 'Renew',
+    VOID = 'Void',
+    ACTIVATE_ITEM = 'activate-item',
+    DRIVER = 'Driver',
+    DELETE_ITEM = 'delete-item',
+    DELETE_DRIVER_MESSAGE = 'Are you sure you want to delete driver(s)?',
+    SEND_MESSAGE = 'Send Message',
+    DM = 'dm',
+    PRINT = 'Print',
+    ACTIVATE = 'Activate',
+    DEACTIVATE = 'Deactivate',
 }
 
 export enum DriverImagesEnum {
     OWNER_STATUS = 'ic_owner-status.svg',
+    EDIT = 'assets/svg/truckassist-table/dropdown/content/edit.svg',
+    RENEW = 'assets/svg/common/ic_reload_renew.svg',
+    CANCEL = 'assets/svg/common/ic_cancel_violation.svg',
+    DELETE = 'assets/svg/common/ic_trash_updated.svg',
+    DM = 'assets/svg/common/ic_dm.svg',
+    PRINT = 'assets/svg/common/ic_fax.svg',
+    DEACTIVATE = 'assets/svg/common/ic_deactivate.svg',
 }
