3e6b2490840486a722f8255128bb7d70f39d9ad9
anndrej7 <andrejnikolic999@gmail.com>
2024-03-29 12:20:34 +0100

    fix

--- src/app/pages/owner/models/owner-table.model.ts
+++ src/app/pages/owner/models/owner-table.model.ts
@@ -1,20 +1,3 @@
-export interface OwnerBackFilterQuery {
-    active: number;
-    companyOwnerId: any;
-    long: number;
-    lat: number;
-    distance: number;
-    truckTypeIds: number[];
-    trailerTypeIds: number[];
-    pageIndex: number;
-    pageSize: number;
-    companyId: number;
-    sort: string;
-    searchOne: string;
-    searchTwo: string;
-    searchThree: string;
-}
-
 export interface MapOwnerData {
     isSelected: boolean;
     textType: string;
