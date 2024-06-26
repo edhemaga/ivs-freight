6f3900e844f10eb465777e66bb6bb9663e19a93f
anndrej7 <andrejnikolic999@gmail.com>
2024-06-26 10:36:19 +0200

    fix

--- src/app/pages/load/pages/load-card/utils/svg-routes/load-card-svg-routes.ts
+++ src/app/pages/load/pages/load-card/utils/svg-routes/load-card-svg-routes.ts
@@ -1,3 +1,5 @@
 export class LoadCardSvgRoutes {
     static attachment = '/assets/svg/truckassist-table/attachment-icon.svg';
+    static trailerImages = '/assets/svg/common/trailers/';
+    static truckImages = '/assets/svg/common/trucks/';
 }
