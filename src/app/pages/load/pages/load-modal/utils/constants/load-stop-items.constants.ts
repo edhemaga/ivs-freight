ab33617fb036aea33b666a7e43a61d2ccf1129d0
tknezevic <teo.hr.knezevic@gmail.com>
2024-07-12 16:05:07 +0200

    Add input config

--- src/app/pages/load/pages/load-modal/utils/constants/load-stop-items.constants.ts
+++ src/app/pages/load/pages/load-modal/utils/constants/load-stop-items.constants.ts
@@ -1,42 +1,107 @@
-// enums
-import { LoadModalStopItemsStringEnum } from '@pages/load/enums/load-modal-stop-items-string.enum';
-
 // models
 import { LoadItemStop } from '@pages/load/pages/load-modal/models/load-item-stop.model';
+import { ITaInput } from '@shared/components/ta-input/config/ta-input.config';
 
 export class LoadStopItems {
-    static STOP_ITEM_HEADERS: string[] = [
-        LoadModalStopItemsStringEnum.HASH,
-        LoadModalStopItemsStringEnum.DESCRIPTION,
-        LoadModalStopItemsStringEnum.QUANTITY,
-        LoadModalStopItemsStringEnum.TMP,
-        LoadModalStopItemsStringEnum.WEIGHT,
-        LoadModalStopItemsStringEnum.LENGTH,
-        LoadModalStopItemsStringEnum.HEIGHT,
-        LoadModalStopItemsStringEnum.TARP,
-        LoadModalStopItemsStringEnum.STACK,
-        LoadModalStopItemsStringEnum.SECURE,
-        LoadModalStopItemsStringEnum.BOL_NO,
-        LoadModalStopItemsStringEnum.PICKUP_NO,
-        LoadModalStopItemsStringEnum.SEAL_NO,
-        LoadModalStopItemsStringEnum.CODE,
-    ];
+    static DESCRIPTION_INPUT_CONFIG: ITaInput = {
+        name: 'Description',
+        type: 'text',
+        label: 'Description',
+        placeholderInsteadOfLabel: true,
+        isRequired: true,
+        hideErrorMessage: true,
+    };
 
-    static IS_INPUT_HOVER_ROW: boolean[] = [
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-        false,
-    ];
+    static QUANTITY_INPUT_CONFIG: ITaInput = {
+        name: 'Input Dropdown',
+        type: 'text',
+        isDropdown: true,
+        label: 'Quantity',
+        placeholderInsteadOfLabel: true,
+        dropdownWidthClass: 'w-col-130',
+    };
+
+    static BOL_INPUT_CONFIG: ITaInput = {
+        name: 'BOL NO',
+        type: 'text',
+        label: 'BOL NO',
+        placeholderInsteadOfLabel: true,
+    };
+    static WEIGHT_INPUT_CONFIG: ITaInput = {
+        name: 'Weight',
+        type: 'text',
+        label: 'Weight',
+        placeholderInsteadOfLabel: true,
+        fixedPlacholder: 'lbs',
+    };
+
+    static LENGTH_INPUT_CONFIG: ITaInput = {
+        name: 'Length',
+        type: 'text',
+        label: 'Length',
+        placeholderInsteadOfLabel: true,
+        fixedPlacholder: 'ft',
+    };
+    static HEIGHT_INPUT_CONFIG: ITaInput = {
+        name: 'Height',
+        type: 'text',
+        label: 'Height',
+        placeholderInsteadOfLabel: true,
+        fixedPlacholder: 'ft',
+    };
+    static TARP_INPUT_CONFIG: ITaInput = {
+        name: 'Tarp',
+        type: 'text',
+        isDropdown: true,
+        label: 'Tarp',
+        placeholderInsteadOfLabel: true,
+        dropdownWidthClass: 'w-col-130',
+    };
+
+    static SECURE_INPUT_CONFIG: ITaInput = {
+        name: 'Input Dropdown',
+        type: 'text',
+        isDropdown: true,
+        label: 'Secure',
+        placeholderInsteadOfLabel: true,
+        dropdownWidthClass: 'w-col-100',
+    };
+
+    static PICKUP_INPUT_CONFIG: ITaInput = {
+        name: 'Pickup No.',
+        type: 'text',
+        label: 'Pickup No.',
+        placeholderInsteadOfLabel: true,
+    };
+
+    static CODE_INPUT_CONFIG: ITaInput = {
+        name: 'Code',
+        type: 'text',
+        label: 'Code',
+        placeholderInsteadOfLabel: true,
+    };
+
+    static SEAL_NUMBER_INPUT_CONFIG: ITaInput = {
+        name: 'Seal No.',
+        type: 'text',
+        label: 'Seal No.',
+        placeholderInsteadOfLabel: true,
+    };
+
+    static STACKABLE_INPUT_CONFIG: ITaInput = {
+        name: 'Input Dropdown',
+        type: 'text',
+        isDropdown: true,
+        label: 'Stack',
+        placeholderInsteadOfLabel: true,
+        dropdownWidthClass: 'w-col-80',
+    };
+    static TEMPERATURE_INPUT_CONFIG: ITaInput = {
+        name: 'TMP',
+        type: 'text',
+        label: 'TMP',
+        placeholderInsteadOfLabel: true,
+    };
 
     static IS_CREATED_NEW_STOP_ITEMS_ROW: LoadItemStop = {
         pickup: false,
--- src/app/shared/components/ta-modal-table/components/ta-modal-table-load-items/ta-modal-table-load-items.component.ts
+++ src/app/shared/components/ta-modal-table/components/ta-modal-table-load-items/ta-modal-table-load-items.component.ts
@@ -7,6 +7,9 @@ import {
 } from '@angular/forms';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
+// Const
+import { LoadStopItems } from '@pages/load/pages/load-modal/utils/constants/load-stop-items.constants';
+
 // Enums
 import { TaModalTableStringEnum } from '../../enums/ta-modal-table-string.enum';
 
@@ -15,6 +18,8 @@ import { TrackByPropertyPipe } from '@shared/pipes/track-by-property.pipe';
 
 // Models
 import { LoadStopItemDropdownLists } from '@pages/load/pages/load-modal/models/load-stop-item-dropdowns-list.model';
+import { ITaInput } from '@shared/components/ta-input/config/ta-input.config';
+import { EnumValue } from 'appcoretruckassist';
 
 // Components
 import { TaInputDropdownComponent } from '@shared/components/ta-input-dropdown/ta-input-dropdown.component';
@@ -43,12 +48,31 @@ export class TaModalTableLoadItemsComponent implements OnInit {
     @Input() arrayName: TaModalTableStringEnum;
     @Input() isInputHoverRows: boolean[][];
     @Input() stopItemDropdownLists: LoadStopItemDropdownLists;
-    @Input()  selectedQuantity = [];
-    @Input()  selectedStack = [];
-    @Input()  selectedSecure = [];
-    @Input()  selectedTarps = [];
+    @Input() selectedQuantity: EnumValue[] = [];
+    @Input() selectedStack: EnumValue[] = [];
+    @Input() selectedSecure: EnumValue[] = [];
+    @Input() selectedTarps: EnumValue[] = [];
     @Output() deleteFormArrayRowClick: EventEmitter<number> =
         new EventEmitter();
+
+    // input configurations
+    public descriptionInputConfig: ITaInput =
+        LoadStopItems.DESCRIPTION_INPUT_CONFIG;
+    public quantityInputConfig: ITaInput = LoadStopItems.QUANTITY_INPUT_CONFIG;
+    public bolInputConfig: ITaInput = LoadStopItems.BOL_INPUT_CONFIG;
+    public weightInputConfig: ITaInput = LoadStopItems.WEIGHT_INPUT_CONFIG;
+    public lengthInputConfig: ITaInput = LoadStopItems.LENGTH_INPUT_CONFIG;
+    public heightInputConfig: ITaInput = LoadStopItems.HEIGHT_INPUT_CONFIG;
+    public tarpInputConfig: ITaInput = LoadStopItems.TARP_INPUT_CONFIG;
+    public codeInputConfig: ITaInput = LoadStopItems.CODE_INPUT_CONFIG;
+    public sealNumberInputConfig: ITaInput =
+        LoadStopItems.SEAL_NUMBER_INPUT_CONFIG;
+    public pickupInputConfig: ITaInput = LoadStopItems.PICKUP_INPUT_CONFIG;
+    public secureInputConfig: ITaInput = LoadStopItems.SECURE_INPUT_CONFIG;
+    public stackableInputConfig: ITaInput =
+        LoadStopItems.STACKABLE_INPUT_CONFIG;
+    public temperatureInputConfig: ITaInput =
+        LoadStopItems.TEMPERATURE_INPUT_CONFIG;
 
     constructor() {}
 
