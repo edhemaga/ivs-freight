50b11dfd90d378f5486def5f11f06108713e8cc6
Tarik Alikadic <tarik.alikadic@carriera.dev>
2024-07-16 08:21:07 +0200

    color finder

--- src/app/pages/dispatch/pipes/color-finder.pipe.ts
+++ src/app/pages/dispatch/pipes/color-finder.pipe.ts
@@ -3,18 +3,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 // constants
 import { DispatchConstants } from '@pages/dispatch/utils/constants/dispatch.constants';
 
-// models
-import { TruckTrailer } from '@pages/dispatch/models/truck-trailer.model';
-
 @Pipe({
     name: 'colorFinder',
 })
 export class ColorFinderPipe implements PipeTransform {
     transform(value: string, id: number): string {
-        let searchInArray: TruckTrailer[] | TruckTrailer[] =
-            DispatchConstants.TRAILER_TYPES;
-
-        if (value === 'truck') searchInArray = DispatchConstants.TRUCK_TYPES;
+        const searchInArray =
+            value === 'truck'
+                ? DispatchConstants.TRUCK_TYPES
+                : DispatchConstants.TRAILER_TYPES;
 
         const findedColor = searchInArray.find((item) => item.id == id);
 
