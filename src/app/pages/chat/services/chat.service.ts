7057790a942758491141989838b223673e3ff4fe
Edim Hadžić <edimhadzic98@gmail.com>
2024-07-24 14:44:06 +0200

    Chat servis

--- src/app/pages/chat/services/chat.service.ts
+++ src/app/pages/chat/services/chat.service.ts
@@ -1,5 +1,10 @@
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
+import {
+    HttpClient,
+    HttpEvent,
+    HttpHeaders,
+} from "@angular/common/http";
 
 // Models
 import {
@@ -14,12 +19,23 @@ import {
 // Services
 import { ChatService } from "appcoretruckassist/api/chat.service";
 
+// environment
+import { environment } from 'src/environments/environment';
+
 @Injectable({
     providedIn: 'root'
 })
 export class UserChatService {
 
-    constructor(private chatService: ChatService) { }
+    private token: string = localStorage.getItem('user')
+        ? JSON.parse(localStorage.getItem('user')).token
+        : 0;
+
+    constructor(
+        public http: HttpClient,
+        private chatService: ChatService
+    ) {
+    }
 
     public getCompanyUserList(userType: UserType, searchParam?: string): Observable<CompanyUserForChatListResponse> {
         return this.chatService.apiChatUserListGet(
@@ -52,15 +68,44 @@ export class UserChatService {
         );
     }
 
+
     public sendMessage(
         conversationId: number,
         content: string,
         attachments?: Blob[]
-    ): Observable<CreateResponse> {
-        return this.chatService.apiChatMessagePost(
-            1,
-            conversationId,
-            content,
+    ): Observable<HttpEvent<CreateResponse>> {
+
+        if (!conversationId) return;
+
+        // Data
+        var formData: FormData = new FormData();
+        console.log(formData)
+
+        formData.append('MessageType', String(1));
+        formData.append('ConversationId', String(conversationId));
+        formData.append('Content', content);
+        console.log(formData)
+
+        if (attachments) attachments.forEach(attachment => {
+            formData.append('Attachments', attachment);
+        })
+
+        // Headers
+        const headers = new HttpHeaders({
+            Authorization: `Bearer ${this.token}`,
+        });
+
+        headers.set('Content-Type', 'multipart/form-data');
+        headers.set('Accept', 'application/json');
+
+        return this.http.post(
+            `${environment.API_ENDPOINT}/api/chat/message`,
+            formData,
+            {
+                headers,
+                reportProgress: true,
+                observe: 'events',
+            }
         );
     }
 
