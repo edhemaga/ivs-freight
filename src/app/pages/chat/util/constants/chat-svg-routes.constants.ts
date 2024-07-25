cd9a07997a09cad615e6f0410ffb92bbfcad5c68
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2024-07-25 11:50:49 +0200

    Icons adjustments

--- src/app/pages/chat/util/constants/chat-svg-routes.constants.ts
+++ src/app/pages/chat/util/constants/chat-svg-routes.constants.ts
@@ -1,19 +1,25 @@
 export class ChatSvgRoutes {
+    // General actions
     static searchIcon: string = 'assets/svg/chat/search-icon.svg';
-    static contactStatusIcon: string = 'assets/svg/chat/contact-status-icon.svg';
-    static departmentIcon: string = 'assets/svg/chat/department-icon.svg';
-    static listOptionsIcon: string = 'assets/svg/chat/list-options-icon.svg';
     static informationIcon: string = 'assets/svg/chat/information-icon.svg';
     static plusIcon: string = 'assets/svg/chat/plus-icon.svg';
     static settingsIcon: string = 'assets/svg/chat/settings-icon.svg';
     static burgerMenuIcon: string = 'assets/svg/chat/burger-menu-icon.svg';
+
+    // User list icons
+    static contactStatusIcon: string = 'assets/svg/chat/contact-status-icon.svg';
+    static departmentIcon: string = 'assets/svg/chat/department-icon.svg';
+    static listOptionsIcon: string = 'assets/svg/chat/list-options-icon.svg';
+
+    // Messages and messages actions
     static faceEmojiIcon: string = 'assets/svg/chat/face-smile-emoji-icon.svg';
     static documentIcon: string = 'assets/svg/chat/document-icon.svg';
     static sendMessageIcon: string = 'assets/svg/chat/send-message-icon.svg';
     static unreadMessageIcon: string = 'assets/svg/chat/unread-messages-icon.svg';
+
+    // Cancle
     static darkXIcon: string = 'assets/svg/chat/x-icon/dark-x-icon.svg';
     static darkFocusedXIcon: string = 'assets/svg/chat/x-icon/dark-focused-x-icon.svg';
     static lightXIcon: string = 'assets/svg/chat/x-icon/light-x-icon.svg';
     static lightFocusedXIcon: string = 'assets/svg/chat/x-icon/light-focused-x-icon.svg';
-
 }
