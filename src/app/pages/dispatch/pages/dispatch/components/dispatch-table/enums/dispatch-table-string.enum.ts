86236be36c945d812d578f0bc67d9f3dede9b0d1
Nikola Narancic <nikola.narancic95@gmail.com>
2024-07-25 12:17:30 +0200

    Fix for toolbar header

--- src/app/pages/dispatch/pages/dispatch/components/dispatch-table/enums/dispatch-table-string.enum.ts
+++ src/app/pages/dispatch/pages/dispatch/components/dispatch-table/enums/dispatch-table-string.enum.ts
@@ -8,5 +8,17 @@ export enum DispatchTableStringEnum {
     CIRCLE_ROUTE = 'ic_circle.svg',
     OWNER_STATUS_ROUTE = 'ic_owner-status.svg',
 
-    PARKING = 'parking'
+    PARKING = 'parking',
+
+    BOARD = 'Board',
+    BOARD_2 = 'board',
+    DISPATCH_USER_SELECT = 'dispatchUserSelect',
+    TEAM_BOARD = 'Team Board',
+    ALL_BOARDS = 'All Boards',
+    ACTIVE = 'active',
+    DISPATCH = 'Dispatch',
+    TOGGLE_LOCKED = 'toggle-locked',
+    DROPDOWN = 'dropdown',
+    LOAD_DISPATCHER = 'load-dispatcher',
+    DISPATCHER = 'dispatcher',
 }
