0c2a19d3186b365ff1d9d4babf435432855bf470
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2024-08-12 15:18:12 +0200

    Link recogniti init

---
+++ src/app/pages/chat/utils/helpers/link-recognition.helper.ts
@@ -0,0 +1,9 @@
+export const checkForLink = (string: string): boolean => {
+    var urlPattern = new RegExp('^(https?:\\/\\/)?' + // Protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // Domain
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // Port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // Query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // Fragment locator
+    return !!urlPattern.test(string);
+}
