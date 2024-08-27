b92e185b3203afb8b5f0bec4ad7bb946539245ae
Ognjen00 <ognjen.maric@carriera.dev>
2024-08-27 12:01:03 +0200

    Dispatch board - added Endorsements to Driver column

---
+++ src/app/pages/dispatch/pipes/driver-endorsements.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'driverEndorsementsText',
+    standalone: true,
+})
+export class DriverEndorsementsPipe implements PipeTransform {
+    constructor() {}
+
+    transform(data: string[]) {
+        let endorsementsText = '';
+
+        const filteredData = data?.filter((item) => item);
+
+        if (filteredData?.length) {
+            endorsementsText = '(' + data.join('&middot;') + ')';
+        }
+
+        return endorsementsText;
+    }
+}
