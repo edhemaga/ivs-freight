700a13ec2122d44dd578f0f66e064dbcf10cf7f4
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2024-09-06 15:58:57 +0200

    Resolvers update #2

---
+++ src/app/pages/chat/resolvers/conversation-list/driver/chat-driver.resolver.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { map, Observable } from "rxjs";
+
+// Models
+import { CompanyUserChatResponsePagination } from "appcoretruckassist";
+
+// Service
+import { UserChatService } from "@pages/chat/services/chat.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ChatDriverResolver {
+
+    constructor(private userChatService: UserChatService) { }
+
+    resolve(): Observable<CompanyUserChatResponsePagination> {
+        return this.userChatService.getCompanyUserList('Driver').pipe(
+            map(res => {
+                return res.pagination
+            })
+        );
+    }
+}
