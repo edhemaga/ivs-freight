b5f74bbc0bf463d8c995160c8cd7bee1bb517350
tknezevic <teo.hr.knezevic@gmail.com>
2024-09-06 19:50:17 +0200

    index files + CAR-1237

---
+++ src/app/pages/repair/pages/repair-modals/repair-shop-modal/models/index.ts
@@ -0,0 +1,7 @@
+export * from './repair-shop-modal-actions.model';
+export * from './repair-shop-modal-input.model';
+export * from './repair-shop-modal-service-tabs.model';
+export * from './repair-shop-modal-services.model';
+export * from './repair-shop-modal-tabs.model';
+export * from './repair-shop.model';
+export * from './repair-shop-open-hours.model';
