04995742e3eb0e9d3bf0cc90d36491489e6d02e6
tknezevic <teo.hr.knezevic@gmail.com>
2024-09-06 19:57:14 +0200

    Repair order index files

---
+++ src/app/pages/repair/pages/repair-modals/repair-order-modal/models/index.ts
@@ -0,0 +1,9 @@
+
+export * from './add-update-repair-properties.model';
+export * from './extended-repair-shop-response.model';
+export * from './extended-service-type-response.model';
+export * from './extended-truck-minimal-response.model';
+export * from './extended-trailer-minimal-response.model';
+export * from './repair-types.model';
+export * from './repair-subtotal.model';
+export * from './subtotal.model';
