77920160edf7d731bc3fe75165e09cc658ec3f43
Ognjen00 <ognjen.maric@carriera.dev>
2024-09-27 13:52:29 +0200

    CAR-1671 - load modal extra stop fix

--- src/app/pages/load/pages/load-modal/load-modal.component.ts
+++ src/app/pages/load/pages/load-modal/load-modal.component.ts
@@ -5358,8 +5358,9 @@ export class LoadModalComponent implements OnInit, OnDestroy, DoCheck {
                             stopType: extraStop.stopType.name,
                             stopOrder: extraStop.stopLoadOrder,
                             stopLoadOrder: extraStop.stopLoadOrder,
-                            shipperId: extraStop.shipper?.id,
-                            shipperContactId: extraStop.shipperContact?.id,
+                            shipperId: extraStop.shipper?.id ?? null,
+                            shipperContactId:
+                                extraStop.shipperContact?.id ?? null,
                             dateFrom: extraStop
                                 ? this.convertDate(extraStop.dateFrom)
                                 : null,
@@ -5384,7 +5385,7 @@ export class LoadModalComponent implements OnInit, OnDestroy, DoCheck {
 
                     this.loadExtraStopsDateRange[index] = !!extraStop?.dateTo;
 
-                    if (editedShipper)
+                    if (editedShipper?.id)
                         this.onSelectDropdown(
                             editedShipper,
                             LoadModalStringEnum.SHIPPER_EXTRA_STOPS,
