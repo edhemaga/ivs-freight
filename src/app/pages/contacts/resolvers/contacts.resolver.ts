c34f564754c87ab1a91c7694d4acd8da80265758
Tarik Alikadic <tarik.alikadic@carriera.dev>
2024-10-07 14:08:53 +0200

    table config & resolvers fix

--- src/app/pages/contacts/resolvers/contacts.resolver.ts
+++ src/app/pages/contacts/resolvers/contacts.resolver.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@angular/core';
 
-
 import { forkJoin, Observable, tap } from 'rxjs';
 
 // services
-import { TruckassistTableService } from '@shared/services/truckassist-table.service';
 import { ContactsService } from '@shared/services/contacts.service';
 
 // store
-import {
-    ContactState,
-    ContactStore,
-} from '@pages/contacts/state/contact.store';
+import { ContactStore } from '@pages/contacts/state/contact.store';
 
 // models
 import { ContactsTableData } from '@pages/contacts/pages/contacts-table/models/contacts-table-data.model';
@@ -19,35 +14,24 @@ import { ContactsTableData } from '@pages/contacts/pages/contacts-table/models/c
 @Injectable({
     providedIn: 'root',
 })
-export class ContactsResolver  {
+export class ContactsResolver {
     constructor(
         private contactService: ContactsService,
-        private contactStore: ContactStore,
-        private tableService: TruckassistTableService
+        private contactStore: ContactStore
     ) {}
     resolve(): Observable<any> {
         return forkJoin([
             this.contactService.getContacts(null, 1, 25),
-            this.tableService.getTableConfig(19),
             this.contactService.companyContactLabelsColorList(),
             this.contactService.getCompanyContactModal(),
         ]).pipe(
-            tap(([contactPagination, tableConfig, colorRes, contactLabels]) => {
+            tap(([contactPagination, colorRes, contactLabels]) => {
                 localStorage.setItem(
                     'contactTableCount',
                     JSON.stringify({
                         contact: contactPagination.count,
                     })
                 );
-
-                if (tableConfig) {
-                    const config = JSON.parse(tableConfig.config);
-
-                    localStorage.setItem(
-                        `table-${tableConfig.tableType}-Configuration`,
-                        JSON.stringify(config)
-                    );
-                }
 
                 const mappedContactLabels = contactLabels.labels.map((item) => {
                     return { ...item, dropLabel: true };
--- src/app/pages/safety/violation/resolvers/roadside-active.resolver.ts
+++ src/app/pages/safety/violation/resolvers/roadside-active.resolver.ts
@@ -1,33 +1,27 @@
 import { Injectable } from '@angular/core';
 
-
-import { forkJoin, Observable, tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 // services
-import { TruckassistTableService } from '@shared/services/truckassist-table.service';
 import { RoadsideService } from '@pages/safety/violation/services/roadside.service';
 
 // store
-import {
-    RoadsideActiveState,
-    RoadsideActiveStore,
-} from '@pages/safety/violation/state/roadside-state/roadside-active/roadside-active.store';
+import { RoadsideActiveStore } from '@pages/safety/violation/state/roadside-state/roadside-active/roadside-active.store';
+
+// models
+import { RoadsideInspectionListResponse } from 'appcoretruckassist';
 
 @Injectable({
     providedIn: 'root',
 })
-export class RoadsideActiveResolver  {
+export class RoadsideActiveResolver {
     constructor(
         private roadsideService: RoadsideService,
-        private roadsideStore: RoadsideActiveStore,
-        private tableService: TruckassistTableService
+        private roadsideStore: RoadsideActiveStore
     ) {}
-    resolve(): Observable<any> {
-        return forkJoin([
-            this.roadsideService.getRoadsideList(true, 1, 1, 25),
-            this.tableService.getTableConfig(20),
-        ]).pipe(
-            tap(([roadsidePagination, tableConfig]) => {
+    resolve(): Observable<RoadsideInspectionListResponse> {
+        return this.roadsideService.getRoadsideList(true, 1, 1, 25).pipe(
+            tap((roadsidePagination) => {
                 localStorage.setItem(
                     'roadsideTableCount',
                     JSON.stringify({
@@ -35,15 +29,6 @@ export class RoadsideActiveResolver  {
                         inactive: roadsidePagination.inactive,
                     })
                 );
-
-                if (tableConfig) {
-                    const config = JSON.parse(tableConfig.config);
-
-                    localStorage.setItem(
-                        `table-${tableConfig.tableType}-Configuration`,
-                        JSON.stringify(config)
-                    );
-                }
 
                 this.roadsideStore.set(roadsidePagination.pagination.data);
             })
