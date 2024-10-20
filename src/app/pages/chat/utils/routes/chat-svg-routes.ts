6d2525cd3150eb512d2c68a49b99f0675c15984f
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2024-10-20 13:42:00 +0200

    Naslovi

--- src/app/pages/chat/utils/routes/chat-svg-routes.ts
+++ src/app/pages/chat/utils/routes/chat-svg-routes.ts
@@ -48,6 +48,7 @@ export class ChatSvgRoutes {
     static carHaulerIcon: string = 'assets/svg/chat/car-hauler-icon.svg';
     static reeferTruckIcon: string = 'assets/svg/chat/reefer-truck-icon.svg';
     static peopleIcon: string = 'assets/svg/chat/people-icon.svg';
+    static companyUserIcon: string = 'assets/svg/chat/company-user-icon.svg';
 
     // Groups
     static notificationIcon: string = 'assets/svg/chat/notification-icon.svg';
