dfbf081c69f6a753574710a6eeff3ded084e3bb4
TeoKnezevic <teo.knezevic@carriera.dev>
2024-10-30 14:03:11 +0100

    CAR-2273

--- src/app/pages/driver/pages/driver-details/components/driver-details-card/components/driver-details-assign-to-card/driver-details-assign-to-card.component.ts
+++ src/app/pages/driver/pages/driver-details/components/driver-details-card/components/driver-details-assign-to-card/driver-details-assign-to-card.component.ts
@@ -1,11 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // components
 import { TaCustomCardComponent } from '@shared/components/ta-custom-card/ta-custom-card.component';
+import { TaAppTooltipV2Component } from '@shared/components/ta-app-tooltip-v2/ta-app-tooltip-v2.component';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 // models
 import { DriverResponse } from 'appcoretruckassist';
+
+// Const
+import { FilterIconRoutes } from '@shared/components/ta-filter/utils/constants/filter-icons-routes.constants';
 
 @Component({
     selector: 'app-driver-details-assign-to-card',
@@ -15,13 +21,18 @@ import { DriverResponse } from 'appcoretruckassist';
     imports: [
         // modules
         CommonModule,
+        NgbModule,
 
         // components
         TaCustomCardComponent,
+        TaAppTooltipV2Component,
+        SvgIconComponent,
     ],
 })
 export class DriverDetailsAssignToCardComponent {
     @Input() cardData: DriverResponse;
+
+    public svgRoutes = FilterIconRoutes;
 
     constructor() {}
 }
