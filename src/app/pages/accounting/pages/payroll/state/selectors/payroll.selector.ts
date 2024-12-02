e641f50eabf180cc7feb1bf28257a2260c2ca429
Ognjen00 <ognjen.maric@carriera.dev>
2024-12-02 10:12:23 +0100

    Load details - added map

--- src/app/pages/accounting/pages/payroll/state/selectors/payroll.selector.ts
+++ src/app/pages/accounting/pages/payroll/state/selectors/payroll.selector.ts
@@ -101,7 +101,7 @@ export const selectPayrollReportMapData = createSelector(
                         },
                         { lat: loadStop.latitude!, lng: loadStop.longitude! },
                     ],
-                    strokeColor: '#919191',
+                    strokeColor: MapOptionsConstants.routingPathColors.gray,
                     strokeOpacity: 1,
                     strokeWeight: 4,
                     isDashed: !!nextLoadNumber,
@@ -112,7 +112,7 @@ export const selectPayrollReportMapData = createSelector(
                 if (index === 1) {
                     routePaths.push({
                         ...routePath,
-                        strokeColor: '#919191',
+                        strokeColor: MapOptionsConstants.routingPathColors.gray,
                         strokeOpacity: 1,
                         strokeWeight: 2,
                     });
