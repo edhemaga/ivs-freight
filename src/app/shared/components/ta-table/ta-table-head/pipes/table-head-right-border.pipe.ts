06c1a0a1f88aa0f75588566f93a0ac2f84d494f1
Tarik Alikadic <tarik.alikadic@carriera.dev>
2024-12-22 12:20:03 +0100

    bug fixes

---
+++ src/app/shared/components/ta-table/ta-table-head/pipes/table-head-right-border.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+// enums
+import { TableHeadTitleStringEnum } from '@shared/components/ta-table/ta-table-head/enums/table-head-title-string.enum';
+
+@Pipe({ name: 'tableHeadRightBorder', standalone: true })
+export class TableHeadRightBorderPipe implements PipeTransform {
+    transform(tableHeadTitle: string, gridNameTitle: string): boolean {
+        return (
+            gridNameTitle === TableHeadTitleStringEnum.REPAIR &&
+            tableHeadTitle === TableHeadTitleStringEnum.COST
+        );
+    }
+}
