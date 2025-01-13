bf7394388d94644efe1eb48a48df7e2a668b781c
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2025-01-13 18:46:22 +0100

    Comments fix

--- src/app/pages/dashboard/pages/dashboard-performance/utils/constants/index.ts
+++ src/app/pages/dashboard/pages/dashboard-performance/utils/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './dashboard-performance-charts-configuration.constants';
 export * from './dashboard-performance.constants';
+export * from './dashboard-performance-icons-routes.constants';
