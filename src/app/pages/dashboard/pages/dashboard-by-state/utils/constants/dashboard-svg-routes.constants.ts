106cdd716f39cd5ffa75f833fb667fd092f6367a
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2025-01-30 14:17:58 +0100

    Comments fix

--- src/app/pages/dashboard/pages/dashboard-by-state/utils/constants/dashboard-svg-routes.constants.ts
+++ src/app/pages/dashboard/pages/dashboard-by-state/utils/constants/dashboard-svg-routes.constants.ts
@@ -1,4 +1,5 @@
 export class DashboardSvgRoutes {
     public static ROUND_BLUE: string =
         'assets/svg/common/round_blue.svg';
+    public static CLOSE_X: string = 'assets/svg/applicant/close-x.svg';
 }
