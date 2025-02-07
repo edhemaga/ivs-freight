3db9305a4efd6940595e8be917072e44a6fe544c
denisredza <denis.redza@carriera.dev>
2025-02-07 11:41:07 +0100

    CAR-3173: [FE][Load] Add Resolver to Load Module root - Resolved PR Changes - Done

--- src/app/pages/load/resolvers/load.resolver.ts
+++ src/app/pages/load/resolvers/load.resolver.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// enums
+import { eLoadStatusType } from '@pages/load/pages/load-table/enums';
 
 // services
 import { LoadStoreService } from '@pages/load/pages/load-table/services/load-store.service';
-import { eLoadStatusType } from '../pages/load-table/enums';
-import { Observable } from 'rxjs';
+
+// models
 import { LoadListResponse, LoadTemplateListResponse } from 'appcoretruckassist';
 
 @Injectable({
@@ -14,7 +18,7 @@ export class LoadResolver {
         // services
         private loadStoreService: LoadStoreService
     ) {}
-    
+
     resolve(): Observable<LoadListResponse | LoadTemplateListResponse> {
         this.loadStoreService.dispatchLoadList({
             statusType: eLoadStatusType.Active,
