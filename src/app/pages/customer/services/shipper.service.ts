7e8a2cb2e5c8f5a2b22c1e376a8ebc9845cde625
denisredza <denis.redza@carriera.dev>
2025-02-26 14:26:05 +0100

    CAR-3377: [Shipper] - Create Load Modal Opens After Shipper Edit - Done

--- src/app/pages/customer/services/shipper.service.ts
+++ src/app/pages/customer/services/shipper.service.ts
@@ -31,7 +31,7 @@ import { ShipperService as ShipperMainService } from 'appcoretruckassist';
 
 // enums
 import { TableStringEnum } from '@shared/enums/table-string.enum';
-import { eGeneralActions } from '@shared/enums';
+import { eGeneralActions } from '@shared/enums/general-actions.enum';
 
 @Injectable({
     providedIn: 'root',
