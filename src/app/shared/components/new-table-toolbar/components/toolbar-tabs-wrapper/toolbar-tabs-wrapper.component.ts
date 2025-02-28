b0f562da9550e07a6710513771381b06289ccea1
tknezevic <teo.hr.knezevic@gmail.com>
2025-02-28 10:41:21 +0100

    Add map to toolbar as optional params

--- src/app/shared/components/new-table-toolbar/components/toolbar-tabs-wrapper/toolbar-tabs-wrapper.component.ts
+++ src/app/shared/components/new-table-toolbar/components/toolbar-tabs-wrapper/toolbar-tabs-wrapper.component.ts
@@ -1,10 +1,20 @@
+// External Libraries
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+// Shared Components
 import { ToolbarTabsComponent } from '../toolbar-tabs/toolbar-tabs.component';
-import { TableStringEnum, ToolbarVariant } from '@shared/enums';
-import { ITableData, TableToolbarActions } from '@shared/models';
+
+// Enums 
+import { TableStringEnum, ToolbarVariant } from '@shared/enums'; 
 import { eActiveViewMode } from '@pages/load/pages/load-table/enums';
-import { TableViewConts } from '@shared/utils/constants';
+
+// Models
+import { ITableData, TableToolbarActions } from '@shared/models';
+
+// Shared Utils
+import { createTableViewConts } from '@shared/utils/constants';
+
 
 @Component({
     selector: 'app-toolbar-tabs-wrapper',
@@ -13,16 +23,21 @@ import { TableViewConts } from '@shared/utils/constants';
     templateUrl: './toolbar-tabs-wrapper.component.html',
     styleUrl: './toolbar-tabs-wrapper.component.scss',
 })
-export class ToolbarTabsWrapperComponent {
+export class ToolbarTabsWrapperComponent implements OnInit {
     @Input() viewMode = eActiveViewMode.List;
     @Input() data: ITableData[];
     @Input() selectedTab: ITableData;
+    @Input() shouldAddMap: boolean = false;
     
     @Output() onTabChange = new EventEmitter<TableToolbarActions>();
 
     public tableStringEnum = TableStringEnum;
     public toolbarVariant = ToolbarVariant;
-    public tableViewData = TableViewConts;
+    public tableViewData = [];
+
+    ngOnInit() {
+        this.tableViewData = createTableViewConts(this.shouldAddMap);
+    }
 
     public onToolBarAction(event: TableToolbarActions) {
         this.onTabChange.emit(event);
--- src/app/shared/utils/constants/table-view-constants.ts
+++ src/app/shared/utils/constants/table-view-constants.ts
@@ -1,10 +1,15 @@
-import { TableStringEnum } from "@shared/enums";
+import { TableStringEnum } from '@shared/enums';
 
-export const TableViewConts = [
-    {
-        name: TableStringEnum.LIST,
-    },
-    {
-        name: TableStringEnum.CARD,
-    },
-]
+export function createTableViewConts(shouldAddMap?: boolean) {
+    // Base array of table views
+    const tableViews = [
+        { name: TableStringEnum.LIST },
+        { name: TableStringEnum.CARD },
+    ];
+    
+    if (shouldAddMap) {
+        tableViews.push({ name: TableStringEnum.MAP });
+    }
+
+    return tableViews;
+}
