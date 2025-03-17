87493230ff45939b22830c225c2ac32719288dc2
tknezevic <teo.hr.knezevic@gmail.com>
2025-03-17 14:42:51 +0100

    Format load state

--- src/app/pages/load/pages/load-table/models/load-state.model.ts
+++ src/app/pages/load/pages/load-table/models/load-state.model.ts
@@ -1,27 +1,30 @@
 // models
-import { ILoadGridItem, ILoadTemplateGridItem } from "@pages/load/pages/load-table/models/index"
-import { LoadModalResponse, LoadResponse } from "appcoretruckassist";
-import { IActiveLoadModalData } from "@pages/load/models/active-load-modal-data.model";
-import {IFilterDropdownList} from 'ca-components';
+import {
+    ILoadGridItem,
+    ILoadTemplateGridItem,
+} from '@pages/load/pages/load-table/models/index';
+import { LoadModalResponse, LoadResponse } from 'appcoretruckassist';
+import { IActiveLoadModalData } from '@pages/load/models/active-load-modal-data.model';
+import { IFilterDropdownList } from 'ca-components';
 
 // enums
-import { eLoadStatusType } from "@pages/load/pages/load-table/enums/index"
-import { eActiveViewMode } from "@shared/enums";
+import { eLoadStatusType } from '@pages/load/pages/load-table/enums/index';
+import { eActiveViewMode } from '@shared/enums';
 
 export interface ILoadState {
-    data: ILoadGridItem[] | ILoadTemplateGridItem[], // list entity data
+    data: ILoadGridItem[] | ILoadTemplateGridItem[]; // list entity data
 
-    modal: LoadModalResponse, // static modal data
-    activeModalData: IActiveLoadModalData,
+    modal: LoadModalResponse; // static modal data
+    activeModalData: IActiveLoadModalData;
 
-    pendingCount: number,
-    activeCount: number,
-    closedCount: number,
-    templateCount: number,
+    pendingCount: number;
+    activeCount: number;
+    closedCount: number;
+    templateCount: number;
 
-    canDeleteSelectedDataRows: boolean,
-    selectedTab: eLoadStatusType,
-    activeViewMode: eActiveViewMode, 
+    canDeleteSelectedDataRows: boolean;
+    selectedTab: eLoadStatusType;
+    activeViewMode: eActiveViewMode;
 
     dispatcherList: IFilterDropdownList[];
     statusList: IFilterDropdownList[];
