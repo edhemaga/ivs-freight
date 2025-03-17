762c01dfed828404bc65eafbe61178adb0b9eb89
tknezevic <teo.hr.knezevic@gmail.com>
2025-03-17 16:54:12 +0100

    Add new packages, minor ui changes

--- src/app/pages/miles/pages/miles-table/miles-table.component.ts
+++ src/app/pages/miles/pages/miles-table/miles-table.component.ts
@@ -10,13 +10,19 @@ import { MilesStoreService } from '@pages/miles/state/services/miles-store.servi
 // Components
 import { NewTableComponent } from '@shared/components/new-table/new-table.component';
 import { TaTruckTrailerIconComponent } from '@shared/components/ta-truck-trailer-icon/ta-truck-trailer-icon.component';
+import { CaCheckboxComponent } from 'ca-components';
 
 @Component({
     selector: 'app-miles-table',
     templateUrl: './miles-table.component.html',
     styleUrl: './miles-table.component.scss',
     standalone: true,
-    imports: [CommonModule, NewTableComponent, TaTruckTrailerIconComponent],
+    imports: [
+        CommonModule,
+        NewTableComponent,
+        TaTruckTrailerIconComponent,
+        CaCheckboxComponent,
+    ],
 })
 export class MilesTableComponent {
     constructor(public milesStoreService: MilesStoreService) {}
