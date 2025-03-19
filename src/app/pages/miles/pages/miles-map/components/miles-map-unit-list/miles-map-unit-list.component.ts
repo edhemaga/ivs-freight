0988ddc3d429827e7dd5a9df8cc96b7e80b5caa6
tknezevic <teo.hr.knezevic@gmail.com>
2025-03-19 21:57:37 +0100

    Inital miles list

--- src/app/pages/miles/pages/miles-map/components/miles-map-unit-list/miles-map-unit-list.component.ts
+++ src/app/pages/miles/pages/miles-map/components/miles-map-unit-list/miles-map-unit-list.component.ts
@@ -1,16 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+    MilesByUnitPaginatedStopsResponse,
+    MilesService,
+} from 'appcoretruckassist';
+
+// Pipes
+import { ThousandSeparatorPipe } from '@shared/pipes';
 
 @Component({
     selector: 'app-miles-map-unit-list',
     templateUrl: './miles-map-unit-list.component.html',
     styleUrl: './miles-map-unit-list.component.scss',
     standalone: true,
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        // Pipes
+        ThousandSeparatorPipe,
+    ],
 })
 export class MilesMapUnitListComponent {
     public isStopListExpanded: boolean = false;
 
+    total: MilesByUnitPaginatedStopsResponse;
+    constructor(public milesService: MilesService) {
+        this.milesService.apiMilesUnitGet(null, null, 13).subscribe((data) => {
+            this.total = data;
+            console.log(data);
+        });
+    }
     public toogleStopList(): void {
         this.isStopListExpanded = !this.isStopListExpanded;
     }
