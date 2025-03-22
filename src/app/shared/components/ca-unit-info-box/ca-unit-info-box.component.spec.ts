ad8c0e5cadb4fe0501fc02dfc50b67fb34c588ea
TeoKnezevic <teo.knezevic@carriera.dev>
2025-03-22 19:45:53 +0100

    Fix import

--- src/app/shared/components/ca-unit-info-box/ca-unit-info-box.component.spec.ts
+++ src/app/shared/components/ca-unit-info-box/ca-unit-info-box.component.spec.ts
@@ -1,23 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CaUnitInfoBoxComponent } from './ca-unit-info-box.component';
+import { CaUnitInfoBoxComponent } from '@shared/components/ca-unit-info-box/ca-unit-info-box.component';
 
 describe('CaUnitInfoBoxComponent', () => {
-  let component: CaUnitInfoBoxComponent;
-  let fixture: ComponentFixture<CaUnitInfoBoxComponent>;
+    let component: CaUnitInfoBoxComponent;
+    let fixture: ComponentFixture<CaUnitInfoBoxComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CaUnitInfoBoxComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [CaUnitInfoBoxComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(CaUnitInfoBoxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(CaUnitInfoBoxComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
