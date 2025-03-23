eac0fa5a948604da703f3dd961307b581648a78f
Ognjen00 <ognjen.maric@carriera.dev>
2025-03-24 00:39:33 +0100

    Fix errors

--- src/app/pages/fuel/base-classes/fuel-mixed-base-class.base.ts
+++ src/app/pages/fuel/base-classes/fuel-mixed-base-class.base.ts
@@ -8,15 +8,22 @@ import { FuelMapMixin } from '@pages/fuel/pages/fuel-table/mixins';
 
 // services
 import { FuelService, ModalService } from '@shared/services';
+import { TruckassistTableService } from '@shared/services/truckassist-table.service';
+import { ConfirmationResetService } from '@shared/components/ta-shared-modals/confirmation-reset-modal/services/confirmation-reset.service';
 
 class ConcreteFuelDropdownMenuActionsBase<
     T,
 > extends FuelDropdownMenuActionsBase {
     destroy$: Subject<void>;
+    
     viewData: T[] = [];
+
     fuelService: FuelService;
     modalService: ModalService;
+    tableService: TruckassistTableService;
+    confirmationResetService: ConfirmationResetService;
 
+    updateToolbarDropdownMenuContent(action?: string): void {}
     handleShowMoreAction(): void {}
 }
 
