dda9046dbce7bb9907eefcee7a6624e40b4e2ba6
andrejnikolic7 <andrejnikolic999@gmail.com>
2025-03-25 16:40:27 +0100

    Miles Cards -> Cards Component WIP

--- src/app/shared/components/ta-modal-table/components/table-card-view/table-card-view.component.ts
+++ src/app/shared/components/ta-modal-table/components/table-card-view/table-card-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// models
+import { CardDetails } from '@shared/models';
 @Component({
     selector: 'app-table-card-view',
     templateUrl: './table-card-view.component.html',
@@ -8,15 +11,15 @@ import { CommonModule } from '@angular/common';
     imports: [CommonModule],
 })
 export class TableCardViewComponent {
-    @Input() set frontSide(value) {
-      this._frontSide = value;
-      console.log(value, 'frontside')
-    };
-    @Input() set viewData(value) {
-      console.log(value, 'vaaaaaaaaal')
-      this._viewData = value;
+    @Input() set frontSide(value: any) {
+        //This is WIP will be changed later...
+        this._frontSide = value;
     }
-    _viewData;
-    _frontSide;
+    @Input() set viewData(value: CardDetails[]) {
+        this._viewData = value;
+    }
+
+    public _viewData: CardDetails[];
+    public _frontSide: any; //This is WIP will be changed later...
     constructor() {}
 }
