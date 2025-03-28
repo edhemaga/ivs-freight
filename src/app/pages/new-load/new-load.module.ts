d7a154df56d0f88f4a5f6e31b7505f7ecb56fdde
tknezevic <teo.hr.knezevic@gmail.com>
2025-03-28 14:08:53 +0100

    fix module

---
+++ src/app/pages/new-load/new-load.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewLoadRoutingModule } from '@pages/new-load/load-routing.module';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule, NewLoadRoutingModule],
+})
+export class NewLoadModule {}
