db705d96a2fa0f786f42d6ec5d3213895985d41e
andrejnikolic7 <andrejnikolic999@gmail.com>
2025-04-09 11:57:23 +0200

    load cards

--- src/app/pages/new-load/pages/new-load-cards/new-load-cards.component.ts
+++ src/app/pages/new-load/pages/new-load-cards/new-load-cards.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// services
+import { LoadStoreService } from '@pages/load/pages/load-table/services/load-store.service';
+
+// interfaces
+import { ICardValueData } from '@shared/interfaces';
 
 @Component({
-  selector: 'app-new-load-cards',
-  templateUrl: './new-load-cards.component.html',
-  styleUrl: './new-load-cards.component.scss',
-  standalone: true,
-  imports: [],
+    selector: 'app-new-load-cards',
+    templateUrl: './new-load-cards.component.html',
+    styleUrl: './new-load-cards.component.scss',
+    standalone: true,
+    imports: [CommonModule],
 })
 export class NewLoadCardsComponent {
-
+    // public frontSideData: ICardValueData[] =
+    //     MilesCardDataConfig.FRONT_SIDE_DATA;
+    // public backSideData: ICardValueData[] = MilesCardDataConfig.BACK_SIDE_DATA;
+    
+    constructor(public loadStoreService: LoadStoreService) {}
 }
