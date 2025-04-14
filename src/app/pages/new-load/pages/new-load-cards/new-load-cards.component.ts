95e6b810785f8b8af080d211bec763f5f91930b9
andrejnikolic7 <andrejnikolic999@gmail.com>
2025-04-14 12:02:47 +0200

    load cards

--- src/app/pages/new-load/pages/new-load-cards/new-load-cards.component.ts
+++ src/app/pages/new-load/pages/new-load-cards/new-load-cards.component.ts
@@ -1,23 +1,98 @@
 import { Component } from '@angular/core';
+
+// modules
 import { CommonModule } from '@angular/common';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 // services
 import { LoadStoreService } from '@pages/load/pages/load-table/services/load-store.service';
 
+// components
+import { CaTableCardViewComponent } from 'ca-components';
+
+// pipes
+import {
+    FormatCurrencyPipe,
+    ThousandSeparatorPipe,
+    GetNestedValuePipe,
+} from '@shared/pipes';
+
+import { ModalService } from '@shared/services';
+import { CardColumnsModalComponent } from '@shared/components/card-columns-modal/card-columns-modal.component';
+
+// configs
+import { LoadCardDataConfig } from '@pages/new-load/pages/new-load-cards/utils/configs';
+
 // interfaces
 import { ICardValueData } from '@shared/interfaces';
+
+// enums
+import { eTableCardViewData, TableStringEnum } from '@shared/enums';
+
+// svg-routes
+import { SharedSvgRoutes } from '@shared/utils/svg-routes';
 
 @Component({
     selector: 'app-new-load-cards',
     templateUrl: './new-load-cards.component.html',
     styleUrl: './new-load-cards.component.scss',
     standalone: true,
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        AngularSvgIconModule,
+
+        // components
+        CaTableCardViewComponent,
+
+        // pipes
+        FormatCurrencyPipe,
+        ThousandSeparatorPipe,
+        GetNestedValuePipe,
+    ],
 })
 export class NewLoadCardsComponent {
-    // public frontSideData: ICardValueData[] =
-    //     MilesCardDataConfig.FRONT_SIDE_DATA;
-    // public backSideData: ICardValueData[] = MilesCardDataConfig.BACK_SIDE_DATA;
-    
-    constructor(public loadStoreService: LoadStoreService) {}
+    public frontSideData: ICardValueData[] = LoadCardDataConfig.FRONT_SIDE_DATA;
+    public backSideData: ICardValueData[] = LoadCardDataConfig.BACK_SIDE_DATA;
+
+    // enums
+    public tableCardViewEnums = eTableCardViewData;
+
+    // svg-routes
+    public sharedSvgRoutes = SharedSvgRoutes;
+
+    constructor(
+        private modalService: ModalService,
+        public loadStoreService: LoadStoreService
+    ) {}
+
+    public openColumnsModal(): void {
+        const action = {
+            data: {
+                cardsAllData: LoadCardDataConfig.CARD_ALL_DATA,
+                front_side: this.frontSideData,
+                back_side: this.backSideData,
+                numberOfRows: 4,
+                checked: true,
+            },
+            title: 'test',
+        };
+
+        this.modalService
+            .openModal(
+                CardColumnsModalComponent,
+                { size: TableStringEnum.SMALL },
+                action
+            )
+            .then((result) => {
+                if (result) {
+                    this.frontSideData = result.selectedColumns.front_side
+                        .slice(0, result.selectedColumns.numberOfRows)
+                        .map((front: ICardValueData) => front.inputItem);
+
+                    this.backSideData = result.selectedColumns.back_side
+                        .slice(0, result.selectedColumns.numberOfRows)
+                        .map((back: ICardValueData) => back.inputItem);
+                }
+            });
+    }
 }
---
+++ src/app/pages/new-load/pages/new-load-cards/utils/configs/load-card-data.config.ts
@@ -0,0 +1,204 @@
+import { ICardValueData } from '@shared/interfaces';
+
+export class LoadCardDataConfig {
+    static FRONT_SIDE_DATA: ICardValueData[] = [
+        {
+            title: 'Status',
+            key: 'stopsCount',
+            template: 'textWithTitle',
+            format: 'number',
+        },
+        {
+            title: 'Pickup',
+            key: 'milesEmpty',
+            template: 'textWithTitle',
+            format: 'number',
+        },
+        {
+            title: 'Delivery',
+            key: 'milesLoaded',
+            template: 'textWithTitle',
+            format: 'number',
+        },
+        {
+            title: 'Billing',
+            key: 'milesTotal',
+            template: 'textWithTitle',
+            format: 'number',
+        },
+    ];
+
+    static BACK_SIDE_DATA: ICardValueData[] = [
+        {
+            title: 'Load',
+            key: 'loadCount',
+            template: 'textWithTitle',
+            format: 'number',
+        },
+        {
+            title: 'Stops • Pickup',
+            key: 'stopsPickup.count',
+            percentKey: 'stopsPickup.percent',
+            template: 'doubleTextPercent',
+            format: 'number',
+        },
+        {
+            title: 'Stops • Delivery',
+            key: 'stopsDelivery.count',
+            percentKey: 'stopsDelivery.percent',
+            template: 'doubleTextPercent',
+            format: 'number',
+        },
+        {
+            title: 'Revenue',
+            key: 'revenue',
+            template: 'textWithTitle',
+            format: 'currency',
+        },
+    ];
+
+    static CARD_ALL_DATA: ICardValueData[] = [
+        {
+            title: 'Type',
+            key: 'truckType.name',
+            image: 'truckType.logoName',
+            template: 'textWithImage',
+            format: 'text',
+        },
+        {
+            isDropdown: true,
+            title: 'Stops',
+            values: [
+                {
+                    title: 'Stops • Count',
+                    secondTitle: 'Count',
+                    key: 'stopsCount',
+                    template: 'textWithTitle',
+                    format: 'number',
+                },
+                {
+                    title: 'Stops • Pickup',
+                    secondTitle: 'Pickup',
+                    key: 'stopsPickup.count',
+                    percentKey: 'stopsPickup.percent',
+                    template: 'doubleTextPercent',
+                    format: 'number',
+                },
+                {
+                    title: 'Stops • Delivery',
+                    secondTitle: 'Delivery',
+                    key: 'stopsDelivery.count',
+                    percentKey: 'stopsDelivery.percent',
+                    template: 'doubleTextPercent',
+                    format: 'number',
+                },
+                {
+                    title: 'Stops • Fuel',
+                    secondTitle: 'Fuel',
+                    key: 'fuelCount?.count',
+                    percentKey: 'fuelCount.percent',
+                    template: 'doubleTextPercent',
+                    format: 'number',
+                },
+                {
+                    title: 'Stops • Parking',
+                    secondTitle: 'Parking',
+                    key: 'parkingCount?.count',
+                    percentKey: 'parkingCount.percent',
+                    template: 'doubleTextPercent',
+                    format: 'number',
+                },
+                {
+                    title: 'Stops • Deadhead',
+                    secondTitle: 'Deadhead',
+                    key: 'deadHeadCount?.count',
+                    percentKey: 'deadHeadCount.percent',
+                    template: 'doubleTextPercent',
+                    format: 'number',
+                },
+                {
+                    title: 'Stops • Repair',
+                    secondTitle: 'Repair',
+                    key: 'repairCount?.count',
+                    percentKey: 'repairCount.percent',
+                    template: 'doubleTextPercent',
+                    format: 'number',
+                },
+                {
+                    title: 'Stops • Towing',
+                    secondTitle: 'Towing',
+                    key: 'towingCount?.count',
+                    percentKey: 'towingCount.percent',
+                    template: 'doubleTextPercent',
+                    format: 'number',
+                },
+            ],
+        },
+        {
+            title: 'Load',
+            key: 'loadCount',
+            template: 'textWithTitle',
+            format: 'number',
+        },
+        {
+            isDropdown: true,
+            title: 'Fuel',
+            values: [
+                {
+                    title: 'Fuel • Gallon',
+                    secondTitle: 'Gallon',
+                    key: 'fuelGalons',
+                    template: 'textWithTitle',
+                    format: 'number',
+                },
+                {
+                    title: 'Fuel • Cost',
+                    secondTitle: 'Cost',
+                    key: 'fuelCost',
+                    template: 'textWithTitle',
+                    format: 'currency',
+                },
+                {
+                    title: 'Fuel • MPG',
+                    secondTitle: 'MPG',
+                    key: 'fuelMpg',
+                    template: 'fuelMpg',
+                    format: 'number',
+                },
+            ],
+        },
+        {
+            isDropdown: true,
+            title: 'Miles',
+            values: [
+                {
+                    title: 'Miles • Loaded',
+                    secondTitle: 'Loaded',
+                    key: 'milesLoaded',
+                    template: 'textWithTitle',
+                    format: 'number',
+                },
+                {
+                    title: 'Miles • Empty',
+                    secondTitle: 'Empty',
+                    key: 'milesEmpty',
+                    template: 'textWithTitle',
+                    format: 'number',
+                },
+                {
+                    title: 'Miles • Total',
+                    secondTitle: 'Total',
+                    key: 'milesTotal',
+                    template: 'textWithTitle',
+                    format: 'number',
+                },
+            ],
+        },
+        {
+            title: 'Revenue',
+            key: 'revenue',
+            template: 'textWithTitle',
+            format: 'currency',
+        },
+    ];
+}
