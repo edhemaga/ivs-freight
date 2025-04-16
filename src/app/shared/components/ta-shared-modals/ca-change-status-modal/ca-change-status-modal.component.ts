1b62871d3189ca6b30d13bbbd5ef78b052d17be9
Uros Jevtic <uros.jevta@gmail.com>
2025-04-16 14:29:08 +0200

    CAR-3847: Change status modal

---
+++ src/app/shared/components/ta-shared-modals/ca-change-status-modal/ca-change-status-modal.component.ts
@@ -0,0 +1,119 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import {
+    AbstractControl,
+    ReactiveFormsModule,
+    UntypedFormBuilder,
+    UntypedFormGroup,
+    Validators,
+} from '@angular/forms';
+
+// Modules
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+// Routes
+import { SharedSvgRoutes } from '@shared/utils/svg-routes';
+
+// Components
+import {
+    CaInputAddressDropdownComponent,
+    CaLoadStatusComponent,
+    CaModalComponent,
+} from 'ca-components';
+
+// Mixings
+import { AddressMixin } from '@shared/mixins';
+
+// Services
+import { AddressService } from '@shared/services/address.service';
+
+// Enums
+import { LoadStatusEnum } from '@shared/enums';
+
+// Models
+import { AddressEntity, AddressResponse } from 'appcoretruckassist';
+import { IModalData } from 'ca-components';
+
+// Pipes
+import { LoadStatusBackgroundColorPipe } from 'ca-components';
+
+@Component({
+    selector: 'app-ca-change-status-modal',
+    standalone: true,
+    imports: [
+        CommonModule,
+        AngularSvgIconModule,
+        ReactiveFormsModule,
+
+        // Components
+        CaModalComponent,
+        CaInputAddressDropdownComponent,
+        CaLoadStatusComponent,
+
+        // Pipes
+        LoadStatusBackgroundColorPipe,
+    ],
+    templateUrl: './ca-change-status-modal.component.html',
+    styleUrl: './ca-change-status-modal.component.scss',
+})
+export class CaChangeStatusModalComponent
+    extends AddressMixin(
+        class {
+            addressService!: AddressService;
+        }
+    )
+    implements OnInit
+{
+    @Input() modalData!: IModalData;
+    @Input() template!: TemplateRef<null>;
+
+    public locationForm!: UntypedFormGroup;
+    public selectedAddress: AddressResponse;
+    public isFormValid!: boolean;
+
+    public sharedSvgRoutes = SharedSvgRoutes;
+
+    constructor(
+        private ngbActiveModal: NgbActiveModal,
+        private formBuilder: UntypedFormBuilder,
+        public addressService: AddressService
+    ) {
+        super();
+    }
+
+    public get locationFormControl(): AbstractControl {
+        return this.locationForm.get('location');
+    }
+
+    public get isStatusUnassigned(): boolean {
+        return (
+            this.modalData.status?.statusValue?.id === LoadStatusEnum.Unassigned
+        );
+    }
+
+    ngOnInit(): void {
+        if (!this.isStatusUnassigned) this.initLocationForm();
+    }
+
+    public initLocationForm(): void {
+        this.locationForm = this.formBuilder.group({
+            location: [null, [Validators.required]],
+        });
+    }
+
+    public onModalAction(isDoAction?: boolean): void {
+        if (this.isStatusUnassigned) this.ngbActiveModal.close(isDoAction);
+        else if (isDoAction) this.ngbActiveModal.close(this.selectedAddress);
+        else this.ngbActiveModal.close(isDoAction);
+    }
+
+    public onHandleAddress(event: {
+        address: AddressEntity;
+        valid: boolean;
+    }): void {
+        this.isFormValid = event.valid;
+
+        if (event.valid) this.selectedAddress = event;
+    }
+}
--- src/app/shared/utils/svg-routes/shared-svg.routes.ts
+++ src/app/shared/utils/svg-routes/shared-svg.routes.ts
@@ -75,6 +75,10 @@ export class SharedSvgRoutes {
         'assets/svg/common/confirmation/ic_confirmation_modal_trash.svg';
     static CONFIRMATION_DELETE_ICON: string =
         'assets/svg/common/confirmation/ic_confirmation_delete.svg';
+    static CONFIRMATION_INFO_ICON: string =
+        'assets/svg/common/confirmation/ic_confirmation_info2.svg';
+    static CONFIRMATION_CHANGE_STATUS_ICON: string =
+        'assets/svg/common/confirmation/confirmation_change_status.svg';
     // applicant
     static BLUE_CHECKMARK: string = 'assets/svg/applicant/check.svg';
 }
