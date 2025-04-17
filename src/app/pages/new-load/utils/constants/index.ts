43742f44917f3db0b3d423f4a06a7bc6a2c30c4f
Denis Truhlaz <d.truhlaz@gmail.com>
2025-04-17 22:49:10 +0200

    fix: rename

--- src/app/pages/new-load/utils/constants/index.ts
+++ src/app/pages/new-load/utils/constants/index.ts
@@ -1,4 +1,4 @@
 export * from './load-details-dropdown.constants';
 export * from './load-store.constants';
 export * from './load-toolbar-tabs.constants';
-export * from './load-type-icon.constants';
+export * from './load-type.constants';
