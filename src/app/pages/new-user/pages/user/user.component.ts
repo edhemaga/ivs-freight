c3115865030051c9dd2483a0d942a7d3c3534e5b
TeoKnezevic <teo.knezevic@carriera.dev>
2025-04-18 19:12:39 +0200

    Inital state setup

---
+++ src/app/pages/new-user/pages/user/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.scss',
+    standalone: true,
+    imports: [],
+})
+export class UserComponent {}
