a7adceb871711038799a1ae346ac234cf4a60f98
tknezevic <teo.hr.knezevic@gmail.com>
2025-04-19 18:43:38 +0200

    Design delete user modal window

--- src/app/pages/new-user/modals/delete-user/delete-user.component.ts
+++ src/app/pages/new-user/modals/delete-user/delete-user.component.ts
@@ -5,14 +5,14 @@ import { Component, Input } from '@angular/core';
 import { IUserDeleteModal } from '@pages/new-user/interfaces';
 
 // Components
-import { CaDeleteModalComponent } from 'ca-components';
+import { CaDeleteModalComponent, CaProfileImageComponent } from 'ca-components';
 
 @Component({
     selector: 'app-delete-user',
     standalone: true,
     templateUrl: './delete-user.component.html',
     styleUrl: './delete-user.component.scss',
-    imports: [CommonModule, CaDeleteModalComponent],
+    imports: [CommonModule, CaDeleteModalComponent, CaProfileImageComponent],
 })
 export class DeleteUserComponent {
     @Input() modalData: IUserDeleteModal;
