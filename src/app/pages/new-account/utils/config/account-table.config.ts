892692a112aac6deb896e7f956cfd35d69ecc509
Denis Truhlaz <d.truhlaz@gmail.com>
2025-05-07 20:45:00 +0200

    disable eslint rule for config class

--- src/app/pages/new-account/utils/config/account-table.config.ts
+++ src/app/pages/new-account/utils/config/account-table.config.ts
@@ -3,7 +3,35 @@ import { CompanyAccountSortBy } from 'appcoretruckassist/model/models';
 // Interfaces
 import { ITableColumn } from '@shared/components/new-table/interfaces';
 
+/* eslint perfectionist/sort-classes: 0 */
 export class AccountTableColumnsConfig {
+    static getTableColumns(): ITableColumn[] {
+        return [
+            AccountTableColumnsConfig.selectColumn,
+            AccountTableColumnsConfig.accountNameColumn,
+            AccountTableColumnsConfig.accountUrlColumn,
+            AccountTableColumnsConfig.accountUsernameColumn,
+            AccountTableColumnsConfig.accountPassowordColumn,
+            AccountTableColumnsConfig.labelColumn,
+            AccountTableColumnsConfig.dateAddedColumn,
+            AccountTableColumnsConfig.dateEditedColumn,
+        ];
+    }
+
+    static selectColumn: ITableColumn = {
+        id: 1,
+        key: 'select',
+        label: '',
+        pinned: 'left',
+        width: 32,
+        minWidth: 32,
+        maxWidth: 200,
+        isResizable: true,
+        isDisabled: true,
+        isChecked: true,
+        hasSort: false,
+    };
+
     static accountNameColumn: ITableColumn = {
         id: 2,
         key: 'name',
@@ -19,20 +47,7 @@ export class AccountTableColumnsConfig {
         hasSort: true,
         sortName: CompanyAccountSortBy.Name,
     };
-    static accountPassowordColumn: ITableColumn = {
-        id: 5,
-        key: 'password',
-        label: 'Password',
-        labelToolbar: 'Password',
-        pinned: null,
-        width: 208,
-        minWidth: 90,
-        maxWidth: 308,
-        isResizable: true,
-        isChecked: true,
-        isDisabled: false,
-        hasSort: false,
-    };
+
     static accountUrlColumn: ITableColumn = {
         id: 3,
         key: 'url',
@@ -48,6 +63,7 @@ export class AccountTableColumnsConfig {
         hasSort: true,
         sortName: CompanyAccountSortBy.Url,
     };
+
     static accountUsernameColumn: ITableColumn = {
         id: 4,
         key: 'username',
@@ -63,36 +79,22 @@ export class AccountTableColumnsConfig {
         hasSort: true,
         sortName: CompanyAccountSortBy.UserName,
     };
-    static dateAddedColumn: ITableColumn = {
-        id: 7,
-        key: 'dateAdded',
-        label: 'Added',
-        labelToolbar: 'Date Added',
+
+    static accountPassowordColumn: ITableColumn = {
+        id: 5,
+        key: 'password',
+        label: 'Password',
+        labelToolbar: 'Password',
         pinned: null,
-        width: 88,
-        minWidth: 72,
-        maxWidth: 88,
+        width: 208,
+        minWidth: 90,
+        maxWidth: 308,
         isResizable: true,
-        isChecked: false,
+        isChecked: true,
         isDisabled: false,
-        hasSort: true,
-        sortName: CompanyAccountSortBy.AddedDate,
+        hasSort: false,
     };
-    static dateEditedColumn: ITableColumn = {
-        id: 8,
-        key: 'dateEdited',
-        label: 'Edited',
-        labelToolbar: 'Date Edited',
-        pinned: null,
-        width: 88,
-        minWidth: 72,
-        maxWidth: 88,
-        isResizable: true,
-        isChecked: false,
-        isDisabled: false,
-        hasSort: true,
-        sortName: CompanyAccountSortBy.EditedDate,
-    };
+
     static labelColumn: ITableColumn = {
         id: 6,
         key: 'label',
@@ -108,30 +110,36 @@ export class AccountTableColumnsConfig {
         hasSort: true,
         sortName: CompanyAccountSortBy.Label,
     };
-    static selectColumn: ITableColumn = {
-        id: 1,
-        key: 'select',
-        label: '',
-        pinned: 'left',
-        width: 32,
-        minWidth: 32,
-        maxWidth: 200,
+
+    static dateAddedColumn: ITableColumn = {
+        id: 7,
+        key: 'dateAdded',
+        label: 'Added',
+        labelToolbar: 'Date Added',
+        pinned: null,
+        width: 88,
+        minWidth: 72,
+        maxWidth: 88,
         isResizable: true,
-        isDisabled: true,
-        isChecked: true,
-        hasSort: false,
+        isChecked: false,
+        isDisabled: false,
+        hasSort: true,
+        sortName: CompanyAccountSortBy.AddedDate,
     };
 
-    static getTableColumns(): ITableColumn[] {
-        return [
-            AccountTableColumnsConfig.selectColumn,
-            AccountTableColumnsConfig.accountNameColumn,
-            AccountTableColumnsConfig.accountUrlColumn,
-            AccountTableColumnsConfig.accountUsernameColumn,
-            AccountTableColumnsConfig.accountPassowordColumn,
-            AccountTableColumnsConfig.labelColumn,
-            AccountTableColumnsConfig.dateAddedColumn,
-            AccountTableColumnsConfig.dateEditedColumn,
-        ];
-    }
+    static dateEditedColumn: ITableColumn = {
+        id: 8,
+        key: 'dateEdited',
+        label: 'Edited',
+        labelToolbar: 'Date Edited',
+        pinned: null,
+        width: 88,
+        minWidth: 72,
+        maxWidth: 88,
+        isResizable: true,
+        isChecked: false,
+        isDisabled: false,
+        hasSort: true,
+        sortName: CompanyAccountSortBy.EditedDate,
+    };
 }
