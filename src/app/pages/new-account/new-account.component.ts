42879b893c8166be2af2ff349d4dfa365b6d5e47
Denis Truhlaz <d.truhlaz@gmail.com>
2025-05-07 15:01:37 +0200

    feat: initial table for new account

--- src/app/pages/new-account/new-account.component.ts
+++ src/app/pages/new-account/new-account.component.ts
@@ -1,14 +1,126 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import {
+    eCommonElement,
+    eDropdownMenuColumns,
+    eGeneralActions,
+    eStatusTab,
+} from '@shared/enums';
+
+import { AccountStoreService } from './state/services/account-store.service';
+
 // Components
 import { AccountTableComponent } from '@pages/new-account/pages/account-table/account-table.component';
+import { ToolbarTabsWrapperComponent } from '@shared/components/new-table-toolbar/components/toolbar-tabs-wrapper/toolbar-tabs-wrapper.component';
+import { NewTableToolbarComponent } from '@shared/components/new-table-toolbar/new-table-toolbar.component';
+import { TaAppTooltipV2Component } from '@shared/components/ta-app-tooltip-v2/ta-app-tooltip-v2.component';
+import { SvgIconComponent } from 'angular-svg-icon';
+import {
+    CaCheckboxSelectedCountComponent,
+    CaFilterListDropdownComponent,
+    CaSearchMultipleStates2Component,
+} from 'ca-components';
+
+import { TableCardBodyActions, TableToolbarActions } from '@shared/models';
+
+import { SharedSvgRoutes } from '@shared/utils/svg-routes';
 
 @Component({
     selector: 'app-new-account',
     standalone: true,
-    imports: [CommonModule, AccountTableComponent],
+    providers: [NgbActiveModal],
+
+    imports: [
+        CommonModule,
+        AccountTableComponent,
+        // Components
+        NewTableToolbarComponent,
+        ToolbarTabsWrapperComponent,
+        CaSearchMultipleStates2Component,
+        CaFilterListDropdownComponent,
+        SvgIconComponent,
+        CaCheckboxSelectedCountComponent,
+        TaAppTooltipV2Component,
+    ],
     templateUrl: './new-account.component.html',
     styleUrl: './new-account.component.scss',
 })
-export class NewAccountComponent {}
+export class NewAccountComponent {
+    public eCommonElement = eCommonElement;
+    public eStatusTab = eStatusTab;
+    public generalActions = eGeneralActions;
+    public sharedIcons = SharedSvgRoutes;
+
+    constructor(
+        public accountStoreService: AccountStoreService,
+        private ngbActiveModal: NgbActiveModal
+    ) {}
+
+    private onViewModeChange(viewMode: string): void {
+        this.accountStoreService.dispatchViewModeChange(
+            viewMode as eCommonElement
+        );
+    }
+
+    private setToolbarDropdownMenuColumnsActive(isActive: boolean): void {
+        this.accountStoreService.dispatchSetToolbarDropdownMenuColumnsActive(
+            isActive
+        );
+    }
+
+    private toggleColumnVisibility(columnType: string, isChecked): void {
+        this.accountStoreService.dispatchToggleColumnsVisiblity(
+            columnType,
+            isChecked
+        );
+    }
+
+    // public onDeleteAccountList(accounts: IMappedAccount[]): void {
+    //     this.accountStoreService.dispatchDeleteAccounts(
+    //         { accounts },
+    //         this.ngbActiveModal
+    //     );
+    // }
+    public onSearchQueryChange(query: string[]): void {
+        this.accountStoreService.dispatchSearchChange(query);
+    }
+
+    public onToolBarAction(event: TableToolbarActions): void {
+        if (!event) return;
+
+        const { action, mode } = event;
+
+        switch (action) {
+            case eGeneralActions.OPEN_MODAL:
+                this.accountStoreService.dispatchOpenUserModal(false, null);
+                break;
+
+            case eGeneralActions.VIEW_MODE:
+                this.onViewModeChange(mode);
+                break;
+        }
+    }
+
+    public onToolbarDropdownMenuActions<T>(action: TableCardBodyActions<T>) {
+        const { type, isActive } = action;
+        switch (type) {
+            case eDropdownMenuColumns.OPEN_TYPE:
+                break;
+            case eDropdownMenuColumns.CLOSE_TYPE:
+                this.setToolbarDropdownMenuColumnsActive(false);
+                break;
+            case eDropdownMenuColumns.COLUMNS_TYPE:
+                this.setToolbarDropdownMenuColumnsActive(true);
+                break;
+            case eDropdownMenuColumns.COLUMNS_BACK_TYPE:
+                this.setToolbarDropdownMenuColumnsActive(false);
+                break;
+            default:
+                this.toggleColumnVisibility(type, isActive);
+                break;
+        }
+    }
+}
--- src/app/pages/new-account/state/functions/account-reducer.functions.ts
+++ src/app/pages/new-account/state/functions/account-reducer.functions.ts
@@ -1,19 +1,35 @@
-// Helpers
-import { AccountHelper } from '@pages/new-account/utils/helpers';
+// Models
+import { GetCompanyAccountListResponse } from 'appcoretruckassist';
+
+import { eCardFlipViewMode, eGeneralActions } from '@shared/enums';
+
+import { StoreFunctionsHelper } from '@shared/components/new-table/utils/helpers';
+import { eCommonElement } from 'ca-components';
 
 // Interfaces
 import { IAccountState } from '@pages/new-account/interfaces';
 
-// Models
-import { GetCompanyAccountListResponse } from 'appcoretruckassist';
+// Helpers
+import { AccountHelper } from '@pages/new-account/utils/helpers';
+import {
+    DropdownMenuColumnsActionsHelper,
+    DropdownMenuToolbarContentHelper,
+} from '@shared/utils/helpers/dropdown-menu-helpers';
 
+//#region List
 export function onLoadAccountsSuccess(
     state: IAccountState,
     data: GetCompanyAccountListResponse
 ): IAccountState {
+    const accountList = [
+        ...state.accountList,
+        ...AccountHelper.accountsMapper(data.pagination.data),
+    ];
     return {
         ...state,
-        accountList: AccountHelper.accountsMapper(data.pagination.data),
+        accountList,
+        searchResultsCount: data.count,
+        currentPage: state.currentPage + 1,
     };
 }
 
@@ -24,3 +40,108 @@ export function onLoadAccountsError(state: IAccountState): IAccountState {
         isLoading: false,
     };
 }
+
+//#endregion
+
+//#region Tabs
+export const onViewModeChange = function (
+    state: IAccountState,
+    activeViewMode: eCommonElement
+): IAccountState {
+    const { tableSettings, isToolbarDropdownMenuColumnsActive } = state;
+    return {
+        ...state,
+        activeViewMode,
+        toolbarDropdownMenuOptions:
+            DropdownMenuToolbarContentHelper.getToolbarDropdownMenuContent(
+                activeViewMode,
+                tableSettings.isTableLocked,
+                eCardFlipViewMode.FRONT, // hardcoded - remove this when somone will do cards for account
+                isToolbarDropdownMenuColumnsActive
+            ),
+    };
+};
+//#endregion
+
+//#region Selection
+export const onAccountSelection = function (
+    state: IAccountState,
+    id: number
+): IAccountState {
+    const accountList = state.accountList.map((user) =>
+        user.id === id ? { ...user, isSelected: !user.isSelected } : user
+    );
+    const areAllUsersSelected = state.accountList.every(
+        (account) => account.isSelected
+    );
+
+    return {
+        ...state,
+        accountList,
+        areAllUsersSelected,
+    };
+};
+
+export function onSelectAll(
+    state: IAccountState,
+    action: string
+): IAccountState {
+    const shouldSelectAll =
+        action === eGeneralActions.SELECT_ALL ||
+        action === eGeneralActions.CLEAR_SELECTED
+            ? !state.areAllUsersSelected
+            : true;
+
+    const accountList = state.accountList.map((account) => ({
+        ...account,
+        isSelected: shouldSelectAll,
+    }));
+
+    return {
+        ...state,
+        accountList,
+        areAllUsersSelected: shouldSelectAll,
+    };
+}
+//#endregion
+
+//#region Toolbar Hamburger Menu
+export function setToolbarDropdownMenuColumnsActive(
+    state: IAccountState,
+    isActive: boolean
+): IAccountState {
+    const { activeViewMode, tableSettings, tableColumns } = state;
+    const toolbarDropdownColumns =
+        DropdownMenuColumnsActionsHelper.mapToolbarDropdownColumnsNew(
+            tableColumns
+        );
+    return {
+        ...state,
+        isToolbarDropdownMenuColumnsActive: isActive,
+        toolbarDropdownMenuOptions:
+            DropdownMenuToolbarContentHelper.getToolbarDropdownMenuContent(
+                activeViewMode,
+                tableSettings.isTableLocked,
+                eCardFlipViewMode.FRONT, // hardcoded - remove this when somone will do cards for user
+                isActive,
+                toolbarDropdownColumns
+            ),
+    };
+}
+
+export function toggleColumnVisibility(
+    state: IAccountState,
+    columnKey: string,
+    isActive: boolean
+): IAccountState {
+    const tableColumns = StoreFunctionsHelper.mapColumnsVisibility(
+        state.tableColumns,
+        columnKey,
+        isActive
+    );
+    return {
+        ...state,
+        tableColumns,
+    };
+}
+//#endregion
