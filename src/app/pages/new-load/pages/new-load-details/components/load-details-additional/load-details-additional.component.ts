06e7f34d380af4a7d34e5ea433b4852873a72896
edhemaga <edim.hadzic@stu.ssst.edu.ba>
2025-05-07 10:56:26 +0200

    Removed code and simplified logic

--- src/app/pages/new-load/pages/new-load-details/components/load-details-additional/load-details-additional.component.ts
+++ src/app/pages/new-load/pages/new-load-details/components/load-details-additional/load-details-additional.component.ts
@@ -7,7 +7,7 @@ import {
     OnInit,
     Output,
 } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 // Third-party modules
 import { AngularSvgIconModule } from 'angular-svg-icon';
@@ -27,33 +27,18 @@ import {
 import { LoadStoreService } from '@pages/new-load/state/services/load-store.service';
 
 // Enums
-import {
-    eGeneralActions,
-    eColor,
-    eDateTimeFormat,
-    eSortDirection,
-    eSharedString,
-} from '@shared/enums';
+import { eColor, eSharedString } from '@shared/enums';
 import { eStringPlaceholder } from 'ca-components';
 import { eLoadDetailsGeneral } from '@pages/new-load/enums';
 
 // Models
-import { CommentCompanyUser, CommentData, Tabs, User } from '@shared/models';
-import {
-    CommentService,
-    CreateCommentCommand,
-    SignInResponse,
-} from 'appcoretruckassist';
-import { ICreateCommentMetadata } from '@pages/load/pages/load-table/models';
+import { Tabs, User } from '@shared/models';
+import { CommentService, SignInResponse } from 'appcoretruckassist';
 import { IDropdownItem } from 'ca-components';
 
 // helpers
-import moment from 'moment';
 import { UserHelper } from '@shared/utils/helpers';
-import {
-    CommentHelper,
-    CommentTabHelper,
-} from '@pages/new-load/pages/new-load-details/utils';
+import { CommentTabHelper } from '@pages/new-load/pages/new-load-details/utils';
 
 // pipes
 import { CreateLoadCommentsPipe } from '@shared/pipes';
@@ -99,6 +84,7 @@ export class LoadDetailsAdditionalComponent implements OnDestroy, OnInit {
     // boolean flags
     public isSearchActive: boolean = false;
     public isDriverButtonSelected: boolean = false;
+    public hasNewComment: Subject<boolean> = new Subject();
 
     // TODO mozda ovo preko nekog id-a odraditi
     public displayedSection:
@@ -136,52 +122,6 @@ export class LoadDetailsAdditionalComponent implements OnDestroy, OnInit {
         this.statusLogSortDirection = sortDirection;
     }
 
-    public onCommentAction(commentData: CommentData, loadId: number): void {
-        switch (commentData.btnType) {
-            case eGeneralActions.CANCEL:
-                break;
-            case eGeneralActions.CONFIRM:
-                const comment = {
-                    ...commentData,
-                    commentDate: `${commentData.commentDate}, ${commentData.commentTime}`,
-                    isCommenting: false,
-                    isEdited: commentData.isEditConfirm,
-                };
-                if (comment.isEdited)
-                    this.editComment(
-                        comment.commentId,
-                        comment.commentContent,
-                        comment.commentDate,
-                        comment.isEdited
-                    );
-                else {
-                    const newComment: CreateCommentCommand =
-                        CommentHelper.createCommentCommand(
-                            loadId,
-                            commentData.commentContent
-                        );
-                    this.commentService
-                        .apiCommentPost(newComment)
-                        .pipe(takeUntil(this.destroy$))
-                        .subscribe();
-                }
-                break;
-            case eGeneralActions.DELETE:
-                // this.loadStoreService.dispatchDeleteCommentById(
-                //     commentData.commentId,
-                //     loadId
-                // );
-                break;
-            default:
-                break;
-        }
-    }
-
-    public onSortAction(sortDirection: eSortDirection, loadId: number): void {
-        if (!sortDirection) return;
-        // this.loadStoreService.sortLoadComments(loadId, sortDirection);
-    }
-
     public onSearchHighlightAction(searchHighlightValue: string): void {
         this.commentFilter = searchHighlightValue;
     }
@@ -190,43 +130,9 @@ export class LoadDetailsAdditionalComponent implements OnDestroy, OnInit {
         this.isSearchActive = !this.isSearchActive;
     }
 
-    public onAddNewComment(loadId: number): void {
-        const comment: CreateCommentCommand =
-            CommentHelper.createCommentCommand(loadId);
-        const dateNow = moment().format(eDateTimeFormat.MM_DD_YY);
-        const { avatarFile, firstName, lastName, companyUserId } =
-            this.companyUser;
-        const commentMetadata: ICreateCommentMetadata = {
-            cardId: loadId,
-            date: dateNow,
-            createdAt: dateNow,
-            companyUser: {
-                avatar: avatarFile.url,
-                fullName: `${firstName} ${lastName}`,
-                id: companyUserId,
-            },
-        };
-        // this.loadStoreService.dispatchCreateComment(comment, commentMetadata);
-    }
-
-    private editComment(
-        commentId: number,
-        commentContent: string,
-        commentDate: string,
-        isEdited: boolean
-    ): void {
-        const updatedComment: CommentCompanyUser = {
-            commentId,
-            commentContent,
-            commentDate,
-            isEdited,
-            companyUser: {
-                id: this.companyUser.userId,
-                name: `${this.companyUser.firstName} ${this.companyUser.lastName}`,
-                avatarFile: this.companyUser.avatarFile,
-            },
-        };
-        // this.loadStoreService.dispatchUpdateComment(updatedComment);
+    public onAddNewComment(): void {
+        this.hasNewComment.next(false);
+        this.hasNewComment.next(true);
     }
 
     public onDropdownItemSelect(event: IDropdownItem): void {
