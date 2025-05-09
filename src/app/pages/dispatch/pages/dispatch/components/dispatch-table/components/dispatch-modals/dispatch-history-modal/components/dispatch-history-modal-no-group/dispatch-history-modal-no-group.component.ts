8b342e5bed9e5009289ab6d8e1c59fc7b1095a18
Tarik Alikadic <tarik.alikadic@carriera.dev>
2025-05-09 11:43:38 +0200

    dispatch board no group click

--- src/app/pages/dispatch/pages/dispatch/components/dispatch-table/components/dispatch-modals/dispatch-history-modal/components/dispatch-history-modal-no-group/dispatch-history-modal-no-group.component.ts
+++ src/app/pages/dispatch/pages/dispatch/components/dispatch-table/components/dispatch-modals/dispatch-history-modal/components/dispatch-history-modal-no-group/dispatch-history-modal-no-group.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+// interfaces
+import { NoGroupItemEmitAction } from '@pages/dispatch/pages/dispatch/components/dispatch-table/interfaces';
 
 @Component({
     selector: 'app-dispatch-history-modal-no-group',
@@ -11,6 +14,9 @@ export class DispatchHistoryModalNoGroupComponent {
     @Input() noGroupData: string[][] = [];
     @Input() noGroupItemSpanArray: number[][] = [];
     @Input() noGroupItemSpanClassNameArray: string[][] = [];
+
+    @Output() onNoGroupItemAction: EventEmitter<NoGroupItemEmitAction> =
+        new EventEmitter();
 
     public hoveredSpanGroup: string;
 
@@ -26,5 +32,39 @@ export class DispatchHistoryModalNoGroupComponent {
         this.hoveredSpanGroup = isHover
             ? this.noGroupItemSpanClassNameArray[rowIndex][colIndex]
             : null;
+    }
+
+    public onNoGroupItemClick(
+        noGroupItem: string,
+        noGroupItemIndex: number,
+        noGroupItemDataIndex: number,
+        noGroupItemSpanClassName: string
+    ): void {
+        const selectedDrodpownListName =
+            this.noGroupHeaderItems[noGroupItemIndex];
+
+        let clickedItem: string;
+
+        if (!noGroupItem) {
+            const firstNoGroupItemDataIndex =
+                this.noGroupItemSpanClassNameArray.findIndex((row) =>
+                    row.includes(noGroupItemSpanClassName)
+                );
+
+            const filteredNoGroupData = this.noGroupData.slice(
+                firstNoGroupItemDataIndex,
+                noGroupItemDataIndex
+            );
+
+            clickedItem = filteredNoGroupData
+                .map((row) => row[noGroupItemIndex])
+                .find((value) => !!value);
+        } else {
+            clickedItem = noGroupItem;
+        }
+
+        const emitAction = { clickedItem, selectedDrodpownListName };
+
+        clickedItem && this.onNoGroupItemAction.emit(emitAction);
     }
 }
